9f418990df82c218dbd8418d1ae17e6509f688ad
bentz <lv.bentz@gmail.com>
2019-03-01 16:54:49 +0800

    首页渲染

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,61 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import { SearchBar } from '@/components'
+import req from '@/api/req'
+import { WgtSlider, WgtNavigation, WgtCoupon, WgtGoodsScroll, WgtGoodsGrid, WgtShowcase } from './wgts'
+
+import './index.scss'
 
 export default class HomeIndex extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      wgts: null
+    }
+  }
+
+  componentWillMount () {
+    this.fetch()
+  }
+
+  async fetch () {
+    const url = 'http://pjj.aixue7.com/index.php/api/wxapp/pageparams/setting?template_name=yykweishop&version=v1.0.1&page_name=index'
+    const info = await req.get(url)
+
+    this.setState({
+      wgts: info.config
+    })
+  }
+
   render () {
+    const { wgts } = this.state
+    if (!wgts) {
+      return null
+    }
+
     return (
-      <View></View>
+      <View className='page-index'>
+        <SearchBar
+          isFixed
+        />
+        <View className='wgts-wrap wgts-wrap__fixed'>
+          {
+            wgts.map((item, idx) => {
+              return (
+                <View className='wgt-wrap' key={idx}>
+                  {item.name === 'slider' && <WgtSlider info={item} />}
+                  {item.name === 'navigation' && <WgtNavigation info={item} />}
+                  {item.name === 'coupon' && <WgtCoupon info={item} />}
+                  {item.name === 'goodsScroll' && <WgtGoodsScroll info={item} />}
+                  {item.name === 'goodsGrid' && <WgtGoodsGrid info={item} />}
+                  {item.name === 'showcase' && <WgtShowcase info={item} />}
+                </View>
+              )
+            })
+          }
+        </View>
+      </View>
     )
   }
 }
---
+++ src/pages/home/wgts/navigation.js
@@ -0,0 +1,47 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text, Image } from '@tarojs/components'
+import { linkPage } from './helper'
+
+import './navigation.scss'
+
+export default class WgtNavigation extends Component {
+  static options = {
+    addGlobalClass: true
+  }
+
+  static defaultProps = {
+    info: null
+  }
+
+  handleClickItem = linkPage
+
+  render () {
+    const { info } = this.props
+    const { base, data } = info
+
+    return (
+      <View className={`wgt ${base.padded ? 'wgt__padded' : null}`}>
+        <View className='wgt-body with-padding'>
+          <View className='navigation'>
+            {data.map((item, idx) => {
+              return (
+                <View
+                  className='nav-item'
+                  key={idx}
+                  onClick={this.handleClickItem}
+                >
+                  <Image
+                    className='nav-img'
+                    mode='widthFix'
+                    src={item.imgUrl}
+                  />
+                  <Text className='nav-name'>{item.content}</Text>
+                </View>
+              )
+            })}
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
