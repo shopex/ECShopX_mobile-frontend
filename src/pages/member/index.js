76accad7b7c0233812bc366148166f1659e65204
anjiaxin <anjiaxin@shopex.cn>
2020-03-31 11:10:43 +0800

    注释入驻相关代码

--- src/pages/member/index.js
+++ src/pages/member/index.js
@@ -436,13 +436,13 @@ export default class MemberIndex extends Component {
               onClick={this.handleClick.bind(this, '/marketing/pages/member/item-activity')}
             >
             </SpCell>
-            <SpCell
+            {/* <SpCell
               title='入驻申请'
               isLink
               img='/assets/imgs/buy.png'
               onClick={this.handleClick.bind(this, '/pages/auth/store-reg')}
             >
-            </SpCell>
+            </SpCell>*/}
 
           </View>
           <View className='page-member-section'>
