ab4aa80f0cd0bd35bdc602e8f66f5ad74fbb87b9
liuhongxu <liuhongxu@shopex.cn>
2020-07-06 15:48:37 +0800

    fix：search-header添加padding时显示错误

--- src/pages/home/wgts/search-home.js
+++ src/pages/home/wgts/search-home.js
@@ -18,15 +18,14 @@ export default class WgtSearchHome extends Component {
       isShowAction: false
     }
   }
-
-  static options = {
-    addGlobalClass: true
-  }
-
   componentDidMount () {
     if (process.env.TARO_ENV === 'h5') {
       toggleTouchMove(this.refs.container)
     }
+  }
+
+  static options = {
+    addGlobalClass: true
   }
 
   searchTap = () => {
@@ -46,9 +45,9 @@ export default class WgtSearchHome extends Component {
     return (
       <View className={`wgt ${base.padded ? 'wgt__padded' : null}`}>
         <View className={`search ${config.fixTop ? 'fixed' : null}`}>
-          <View className="content-padded">
-            <View className="search-box view-flex view-flex-middle view-flex-center" onClick={this.searchTap.bind(this)}>
-              <Icon className="search-icon" type="search" size="14" color="#999999"></Icon>
+          <View className={`content-padded ${base.padded ? 'wgt__padded' : null}`}>
+            <View className='search-box view-flex view-flex-middle view-flex-center' onClick={this.searchTap.bind(this)}>
+              <Icon className='iconfont search-icon' type='search' size='14' color='#999999'></Icon>
               <View>输入商品名称</View>
             </View>
           </View>
