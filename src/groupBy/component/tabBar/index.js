6742ba5cbbaf8149458d569c908847f1cf600b54
liuhongxu <liuhongxu@shopex.cn>
2020-07-06 18:18:11 +0800

    feat: 团购tabbar增加个人中心

--- src/groupBy/component/tabBar/index.js
+++ src/groupBy/component/tabBar/index.js
@@ -3,10 +3,10 @@
  * @GitHub: https://github.com/973749104
  * @Blog: https://liuhgxu.com
  * @Description: 底部菜单栏
- * @FilePath: /feat-Unite-group-by/src/groupBy/component/navBar/index.js
+ * @FilePath: /unite-vshop/src/groupBy/component/tabBar/index.js
  * @Date: 2020-04-29 17:00:48
  * @LastEditors: Arvin
- * @LastEditTime: 2020-04-29 18:14:35
+ * @LastEditTime: 2020-07-06 18:17:14
  */
 import Taro, { Component } from '@tarojs/taro'
 import { AtTabBar } from 'taro-ui'
@@ -24,9 +24,11 @@ export default class NavBar extends Component {
   handleTab = (current) => {
     const tabList = [
       '/groupBy/pages/home/index',
-      '/groupBy/pages/cart/index'
+      '/groupBy/pages/cart/index',
+      '/pages/member/index'
     ]
-    Taro.redirectTo({
+    const jumpType = current === 2 ? 'navigateTo' : 'redirectTo'
+    Taro[jumpType]({
       url: tabList[current]
     })
   }
@@ -41,8 +43,9 @@ export default class NavBar extends Component {
         onClick={this.handleTab}
         backgroundColor='#fafafa'
         tabList={[
-          { title: '首页', iconType: 'home' },
-          { title: '购物车', iconType: 'shopping-cart', text: '' },
+          { title: '首页', iconType: 'home', iconPrefixClass: 'icon' },
+          { title: '购物车', iconType: 'cart', iconPrefixClass: 'icon', text: '' },
+          { title: '会员中心', iconType: 'member', iconPrefixClass: 'icon'},
         ]}
       />
     )
