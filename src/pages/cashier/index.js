8d58309b77837c9a551c5b51769ee2f1766e6f40
liuhongxu <liuhongxu@shopex.cn>
2020-07-20 15:21:06 +0800

    fix: H5适配

--- src/pages/cashier/index.js
+++ src/pages/cashier/index.js
@@ -1,65 +1,74 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import api from '@/api'
-import { Loading, NavBar } from '@/components'
-import { pickBy } from '@/utils'
+import { Loading, NavBar, SpToast } from '@/components'
+import { pickBy, browser } from '@/utils'
 import { withLogin } from '@/hocs'
-import { AlipayPay, WeH5Pay, PointDepositPay, WePay } from './comps'
+import { AlipayPay, WeH5Pay, WePay } from './comps'
+
 
 import './index.scss'
 
 @withLogin()
 export default class Cashier extends Component {
   state = {
-    info: null
+    info: null,
+    env: ""
   }
 
-  componentDidShow () {
+  componentDidShow() {
     this.fetch()
+
   }
 
-  async fetch () {
-    const { order_id } = this.$router.params
+  async fetch() {
+    const { order_id } = this.$router.params;
+
+    let env = ""
+    if (browser.weixin) {
+      env = "WX"
+    }
+
     Taro.showLoading()
-    const orderInfo = await api.cashier.getOrderDetail(order_id)
-    const info = pickBy(orderInfo, {
+    const orderInfo = await api.cashier.getOrderDetail(order_id);
+
+    const info = pickBy(orderInfo.orderInfo, {
       order_id: 'order_id',
       order_type: 'order_type',
       pay_type: 'pay_type',
       point: 'point',
       title: 'title',
-      total_fee: ({ total_fee }) => (total_fee/100).toFixed(2)
+      total_fee: ({ total_fee }) => (total_fee / 100).toFixed(2)
     })
 
     this.setState({
-      info: info
+      info,
+      env
     })
     Taro.hideLoading()
-
   }
 
   handleClickBack = () => {
-    const { order_type } = this.state.info
-    const url = order_type === 'recharge' ? '/pages/member/pay' : '/subpage/pages/trade/list'
+    const { order_type } = this.state.info;
+    const url = order_type === 'recharge' ? '/pages/member/pay' : '/pages/trade/list?redrict=home'
 
     Taro.redirectTo({
       url
     })
   }
 
+  render() {
+    const { info, env, appPay } = this.state;
 
-  render () {
-    const { info } = this.state
-
-    if(!info) {
-      <Loading />
+    if (!info) {
+      return  <Loading />;
     }
 
     return (
       <View className='page-cashier-index'>
         <NavBar
           title='收银台'
-          handleClickBack={this.handleClickBack}
+          onClickLeftIcon={this.handleClickBack}
         />
         <View className='cashier-money'>
           {
@@ -68,38 +77,29 @@ export default class Cashier extends Component {
               : null
           }
           <View className='cashier-money__content'>
-            <View className='cashier-money__content-title'>订单编号： { info.order_id }</View>
-            <View className='cashier-money__content-title'>订单名称：{ info.title }</View>
-            <View className='cashier-money__content-title'>应付总额{ info.pay_type === 'point' ? '（积分）' : '（元）'}</View>
-            <View className='cashier-money__content-number'>{ info.pay_type === 'point' ? info.point : info.total_fee }</View>
+            <View className='cashier-money__content-title'>订单编号： {info.order_id}</View>
+            <View className='cashier-money__content-title'>订单名称：{info.title}</View>
+            <View className='cashier-money__content-title'>应付总额{info.pay_type === 'point' ? '（积分）' : '（元）'}</View>
+            <View className='cashier-money__content-number'>{info.pay_type === 'point' ? info.point : info.total_fee}</View>
           </View>
         </View>
         {
-          info &&
-            <View className='pay-status'>
-              {
-                info.order_type === 'recharge'
-                  ? <View>
-                      <AlipayPay
-                        orderID={info.order_id}
-                        payType='alipayh5'
-                        orderType={info.order_type}
-                      />
-                      <WeH5Pay orderID={info.order_id} />
-                    </View>
-                  : <View>
-                    <PointDepositPay
-                      orderID={info.order_id}
-                      payType={info.pay_type}
-                      orderType={info.order_type}
-                    />
-                    <WePay
-                      info={info}
-                    />
-                  </View>
-              }
+          !env
+          ? <View>
+                <AlipayPay
+                  orderID={info.order_id}
+                  payType='alipayh5'
+                  orderType={info.order_type}
+                />
+                <WeH5Pay orderID={info.order_id} />
+              </View>
+            : <View>
+              <WePay
+                info={info}
+              />
             </View>
         }
+      <SpToast /> 
       </View>
     )
   }
