395b1a9d08a7ba07dbee949705b94ebfbf2f8782
liuhongxu <liuhongxu@shopex.cn>
2020-09-24 14:13:30 +0800

    feat: 支付及支付详情页面开发

--- src/boost/pages/pay/index.js
+++ src/boost/pages/pay/index.js
@@ -6,30 +6,204 @@
  * @FilePath: /unite-vshop/src/boost/pages/pay/index.js
  * @Date: 2020-09-23 16:49:53
  * @LastEditors: Arvin
- * @LastEditTime: 2020-09-23 18:33:40
+ * @LastEditTime: 2020-09-24 11:55:06
  */
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { Textarea, View, Image, Text, Button } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+import { pickBy } from '@/utils'
+import api from '@/api'
+import { NavBar, AddressChoose } from '@/components'
 
 import './index.scss'
 
+
+@connect(({ address }) => ({
+  address: address.current
+}), (dispatch) => ({
+  onAddressChoose: (address) => dispatch({ type: 'address/choose', payload: address })
+}))
 export default class Pay extends Component {
   constructor (props) {
     super(props)
+    this. state = {
+      cur_address: null,
+      remark: '',
+      goodInfo: {},
+      purchasePrice: '0.00',
+      isLoading: false
+    }
   }
 
   componentDidMount () {
     this.getOrderInfo()
   }
 
-  async getOrderInfo () {
-    
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.address !== this.props.address) {
+      console.log(nextProps.address)
+      this.handleChangeAddress(nextProps.address)
+    }
+  }
+
+  config = {
+    navigationBarTitleText: '结算页面'
+  }
+
+  // 处理地址
+  handleChangeAddress = (address) => {
+    if (!address) return false
+    const formatAddress = pickBy(address, {
+      state: 'province',
+      city: 'city',
+      district: 'county',
+      address_id: 'address_id',
+      mobile: 'telephone',
+      name: 'username',
+      zip: 'postalCode',
+      address: 'adrdetail',
+      area: 'area'
+    })
+    this.setState({
+      cur_address: formatAddress
+    })
+  }
+
+  // 输入备注
+  remarkInput = (e) => {
+    const { detail = {} } = e
+    const { value = '' } = detail
+    this.setState({
+      remark: value
+    })
+  }
+
+  // 获取数据
+  getOrderInfo = async () => {
+    const { bargain_id } = this.$router.params
+    const {
+      bargain_info = {},
+      user_bargain_info = {}
+    } = await api.boost.getUserBargain({
+      bargain_id,
+      has_order: true
+    })
+    const { mkt_price: mPrice, price } = bargain_info
+    const { user_id, cutdown_amount } = user_bargain_info
+    let purchasePrice = mPrice
+    const discount = mPrice - price
+    if (user_id && (discount > cutdown_amount)) {
+      purchasePrice = mPrice - cutdown_amount
+    }
+    const { list = [] } = await api.member.addressList()
+    this.setState({
+      goodInfo: pickBy(bargain_info, {
+        bargain_id: 'bargain_id',
+        item_name: 'item_name',
+        item_pics: 'item_pics',
+        item_intro: 'item_intro',
+        bargain_rules: 'bargain_rules',
+        share_msg: 'share_msg',
+        mkt_price: ({ mkt_price }) => (mkt_price / 100).toFixed(2),
+      }),
+      purchasePrice: (purchasePrice / 100).toFixed(2)
+    }, () => {
+      // 设置默认地址
+      let defaultAddress = null
+      if (list && list.length > 0) {
+        const isDef = list.find(item => item.is_def)
+        defaultAddress = isDef
+      }
+      this.props.onAddressChoose(defaultAddress)
+    })
+  }
+
+  // 支付
+  handlePay = async () => {
+    const { cur_address, goodInfo, remark } = this.state
+    if (!cur_address) {
+      Taro.showToast({
+        title: '请选择地址',
+        icon: 'none',
+        mask: true
+      })
+      return false
+    }
+    // loading
+    this.setState({
+      isLoading: true
+    })
+    const receiver = pickBy(cur_address, {
+      receiver_name: 'name',
+      receiver_mobile: 'mobile',
+      receiver_state: 'state',
+      receiver_city: 'city',
+      receiver_district: 'district',
+      receiver_address: 'address',
+      receiver_zip: 'zip'
+    })
+    const param = {
+      order_type: 'bargain',
+      bargain_id: goodInfo.bargain_id,
+      item_num: 1,
+      remark,
+      ...receiver
+    }
+    try {
+      const res = await api.boost.pay(param)
+      if (res.order_id) {
+        const payRes = Taro.requestPayment(res)
+        console.log(payRes)
+      }
+      this.setState({
+        isLoading: false
+      })
+    } catch (e) {
+      this.setState({
+        isLoading: false
+      })
+    }
   }
 
   render () {
+    const { cur_address, remark, goodInfo, purchasePrice, isLoading } = this.state
     return (
-      <View>
-        Pay
+      <View className='pay'>
+        <NavBar
+          title={this.config.navigationBarTitleText}
+          leftIconType='chevron-left'
+          fixed='true'
+        />
+        <AddressChoose isAddress={cur_address} />
+        <View className='remark'>
+          <View className='title'>备注</View>
+          <Textarea 
+            style='width:100%;min-height: 50px'
+            placeholder='例如颜色尺寸等'
+            value={remark}
+            onInput={this.remarkInput.bind(this)}
+            autoHeight
+          />
+        </View>
+        <View className='goods'>
+          <Image src={goodInfo.item_pics} mode='aspectFill' className='img' />
+          <View className='info'>
+            <View className='name'>{ goodInfo.item_name }</View>
+            <View className='price'>
+              <Text>¥{ goodInfo.mkt_price }/件</Text>
+              <Text>x1</Text>
+            </View>
+          </View>
+        </View>
+        <View className='sum'>
+          商品总价<Text>¥{ purchasePrice }</Text>
+        </View>
+        <View className='actBtn'>
+          <View className='price'>
+            实付款: <Text>¥{ purchasePrice }</Text>
+          </View>
+          <Button className='btn' disabled={isLoading} loading={isLoading} onClick={this.handlePay.bind(this)}>立即付款</Button>
+        </View>
       </View>
     )
   }  
--- src/boost/pages/payDetail/index.js
+++ src/boost/pages/payDetail/index.js
@@ -6,22 +6,90 @@
  * @FilePath: /unite-vshop/src/boost/pages/payDetail/index.js
  * @Date: 2020-09-23 16:49:53
  * @LastEditors: Arvin
- * @LastEditTime: 2020-09-23 18:22:32
+ * @LastEditTime: 2020-09-24 14:00:15
  */
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
+import { pickBy, formatDataTime } from '@/utils'
+import api from '@/api'
+import { NavBar } from '@/components'
 
 import './index.scss'
 
 export default class PayDetail extends Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      info: {}
+    }
   }
 
+  componentWillMount () {
+    this.getOrderInfo()
+  }
+
+
+  config = {
+    navigationBarTitleText: '结算页面'
+  }
+  
+  // 获取订单信息
+  getOrderInfo = async () => {
+    const { bargain_id } = this.$router.params
+    const {
+      bargain_order = {}
+    } = await api.boost.getUserBargain({
+      bargain_id,
+      has_order: true
+    })
+    
+    this.setState({
+      info: pickBy(bargain_order, {
+        order_id: 'order_id',
+        bargain_id: 'bargain_id',
+        item_name: 'item_name',
+        item_pics: 'item_pics',
+        item_intro: 'item_intro',
+        bargain_rules: 'bargain_rules',
+        share_msg: 'share_msg',
+        receiver_name: 'receiver_name',
+        receiver_mobile: 'receiver_mobile',
+        receiver_state: 'receiver_state',
+        receiver_city: 'receiver_city',
+        receiver_district: 'receiver_district',
+        receiver_address: 'receiver_address',
+        remark: 'remark',
+        create_time: ({ create_time }) => formatDataTime(create_time),
+        total_fee: ({ total_fee }) => (total_fee / 100).toFixed(2),
+      })
+    })
+  }  
+  
   render () {
+    const { info } = this.state
     return (
-      <View>
-        payDetail
+      <View className='payDetail'>
+        <NavBar
+          title={this.config.navigationBarTitleText}
+          leftIconType='chevron-left'
+          fixed='true'
+        />
+        <View className='address'>
+          <View className='title'>收货信息</View>
+          <View className='info'>
+            <Text>
+              { info.receiver_name }
+            </Text>
+            <Text>
+              { info.receiver_mobile }
+            </Text>
+          </View>
+          <View className='add'>
+            { info.receiver_state}{ info.receiver_city }{ info.receiver_district }
+            { info.receiver_address }
+          </View>
+        </View>
       </View>
     )
   }  
