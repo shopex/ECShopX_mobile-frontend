39460db95e0654ec0ce05e20e6e39a7a0d192f6e
liuhongxu <liuhongxu@shopex.cn>
2020-11-04 15:55:40 +0800

    feat: 上传接口改造

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,40 +1,4 @@
 /*
- *                        ::
- *                       :;J7, :,                        ::;7:
- *                       ,ivYi, ,                       ;LLLFS:
- *                       :iv7Yi                       :7ri;j5PL
- *                      ,:ivYLvr                    ,ivrrirrY2X,
- *                      :;r@Wwz.7r:                :ivu@kexianli.
- *                     :iL7::,:::iiirii:ii;::::,,irvF7rvvLujL7ur
- *                    ri::,:,::i:iiiiiii:i:irrv177JX7rYXqZEkvv17
- *                 ;i:, , ::::iirrririi:i:::iiir2XXvii;L8OGJr71i
- *               :,, ,,:   ,::ir@mingyi.irii:i:::j1jri7ZBOS7ivv,
- *                  ,::,    ::rv77iiiriii:iii:i::,rvLq@huhao.Li
- *              ,,      ,, ,:ir7ir::,:::i;ir:::i:i::rSGGYri712:
- *            :::  ,v7r:: ::rrv77:, ,, ,:i7rrii:::::, ir7ri7Lri
- *           ,     2OBBOi,iiir;r::        ,irriiii::,, ,iv7Luur:
- *         ,,     i78MBBi,:,:::,:,  :7FSL: ,iriii:::i::,,:rLqXv::
- *         :      iuMMP: :,:::,:ii;2GY7OBB0viiii:i:iii:i:::iJqL;::
- *        ,     ::::i   ,,,,, ::LuBBu BBBBBErii:i:i:i:i:i:i:r77ii
- *       ,       :       , ,,:::rruBZ1MBBqi, :,,,:::,::::::iiriri:
- *      ,               ,,,,::::i:  @arqiao.       ,:,, ,:::ii;i7:
- *     :,       rjujLYLi   ,,:::::,:::::::::,,   ,:i,:,,,,,::i:iii
- *     ::      BBBBBBBBB0,    ,,::: , ,:::::: ,      ,,,, ,,:::::::
- *     i,  ,  ,8BMMBBBBBBi     ,,:,,     ,,, , ,   , , , :,::ii::i::
- *     :      iZMOMOMBBM2::::::::::,,,,     ,,,,,,:,,,::::i:irr:i:::,
- *     i   ,,:;u0MBMOG1L:::i::::::  ,,,::,   ,,, ::::::i:i:iirii:i:i:
- *     :    ,iuUuuXUkFu7i:iii:i:::, :,:,: ::::::::i:i:::::iirr7iiri::
- *     :     :rk@Yizero.i:::::, ,:ii:::::::i:::::i::,::::iirrriiiri::,
- *      :      5BMBBBBBBSr:,::rv2kuii:::iii::,:i:,, , ,,:,:i@petermu.,
- *           , :r50EZ8MBBBBGOBBBZP7::::i::,:::::,: :,:,::i;rrririiii::
- *               :jujYY7LS0ujJL7r::,::i::,::::::::::::::iirirrrrrrr:ii:
- *            ,:  :@kevensun.:,:,,,::::i:i:::::,,::::::iir;ii;7v77;ii;i,
- *            ,,,     ,,:,::::::i:iiiii:i::::,, ::::iiiir@xingjief.r;7:i,
- *         , , ,,,:,,::::::::iiiiiiiiii:,:,:::::::::iiir;ri7vL77rrirri::
- *          :,, , ::::::::i:::i:::i:i::,,,,,:,::i:i:::iir;@Secbone.ii:::
- */
-
-/*
  * @Author: Arvin
  * @GitHub: https://github.com/973749104
  * @Blog: https://liuhgxu.com
@@ -42,28 +6,82 @@
  * @FilePath: /unite-vshop/src/utils/upload.js
  * @Date: 2020-03-06 16:32:07
  * @LastEditors: Arvin
- * @LastEditTime: 2020-10-28 10:09:54
+ * @LastEditTime: 2020-11-04 15:53:13
  */
+import Taro from '@tarojs/taro'
+import req from '@/api/req'
+import * as qiniu from 'qiniu-js'
 
-import AliOss from './aliyun'
-import QiNiu from './qiniu'
-// import Tecent from './tecent'
-// 判断上传
+const getToken = (params) => {
+  return req.get('espier/image_upload_token', params)
+}
 
+const upload = {
+  aliUpload: async (item, tokenRes) => {
+    const {accessid, callback = () => {}, dir, host, policy, signature} = tokenRes
+    const filename = item.url.slice(item.url.lastIndexOf('/') + 1)
+    const res = await Taro.uploadFile({
+      url: host,
+      filePath: item.url,
+      name: 'file',
+      formData:{
+        name: filename,
+        key: `${dir}`,
+        policy: policy,
+        OSSAccessKeyId: accessid,
+        // 让服务端返回200
+        signature: signature,
+        success_action_status: '200',
+        // 服务端回调
+        callback: callback
+      }
+    })
+    return {
+      url: `${host}${dir}`
+    }
+  },
+  qiNiuUpload: async (item, tokenRes) => {
+    const {token, key, domain, region } = tokenRes
+    const res = await  Taro.uploadFile({
+      url: uploadUrl,
+      filePath: item.url,
+      name: 'file',
+      formData:{
+        'token': token,
+        'key': key
+      }})
+  },
+  localUpload: async () => {
 
-// console.log(process.env.STORAGE)
+  }
+}
 
-const storage = (STORAGE === 'ali') ? AliOss: QiNiu
+const getUploadFun = (dirver) => {
+  switch (dirver) {
+    case 'oss':
+      return 'aliUpload'
+    case 'local':
+      return 'localUpload'
+    default:
+      return 'qiNiuUpload'
+  }
+}
 
-// switch (process.env.STORAGE) {
-//   case 'ali':
-//     storage = AliOss
-//     break;
-//   case 'tecent':
-//     storage = Tecent
-//     break;
-//   default:
-//     storage = QiNiu
-// }
+// 返回对应上传方式
+const uploadImageFn = async (imgFiles) => {
+  const { driver = 'oss', ...toeknRes } = await getToken({filetype: this.fileType})
+  const uploadType = getUploadFun(driver)
+  const imgs = []
+  for (const item of imgFiles) {
+      if (!item.file) {
+        continue
+      }
+      const img = await uploadType[uploadType](item, toeknRes)
+      imgs.push(img)
+  }
+  return imgs
+}
 
-export default storage
+export default {
+  uploadImageFn: uploadImageFn
+}
