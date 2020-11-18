ab4e534b513da62ca5b1c0f9fdf6d2107fbc2e5d
[wangtengteng] <[wangtengteng@shopex.cn]>
2020-11-18 17:35:59 +0800

    组件修改

--- src/pages/home/wgts/img-gif.js
+++ src/pages/home/wgts/img-gif.js
@@ -1,6 +1,7 @@
 
 import Taro, { Component } from '@tarojs/taro'
 import { View, Image } from '@tarojs/components'
+import { SpImg } from '@/components'
 import './img-gif.scss'
 
 export default class WgtImgGif extends Component {
@@ -21,17 +22,31 @@ export default class WgtImgGif extends Component {
 
         return (
             <View className={`index ${info.base.padded ? 'wgt__padded' : null}`}>
-                <View className='imglist'>
-                    <Image
+                <View className='imglist' style={`background:url(${info.data[0].imgUrl})`}>
+                    {/* <Image
                         mode='widthFix'
                         className='scale-placeholder png'
                         src={info.data[0].imgUrl}
-                    />
-                    <Image
-                        mode='widthFix'
+                    /> */}
+                    {/* <Image
+                        
                         className='scale-placeholder gif'
                         src={info.data[1].imgUrl}
-                    />
+                    /> */}
+                     {/* <SpImg
+                        img-class='scale-placeholder png'
+                        src={info.data[0].imgUrl}
+                        mode='widthFix'
+                        width='750'
+                        lazyLoad
+                      /> */}
+                     <SpImg
+                        img-class='scale-placeholder gif'
+                        src={info.data[1].imgUrl}
+                        mode='widthFix'
+                        width='750'
+                        lazyLoad
+                      />
                 </View >
             </View>
         )
