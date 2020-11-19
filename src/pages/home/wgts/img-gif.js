9d8433bd71e1b78e725f729c5e375719b35481c0
[wangtengteng] <[wangtengteng@shopex.cn]>
2020-11-19 10:54:48 +0800

    bug修复

--- src/pages/home/wgts/img-gif.js
+++ src/pages/home/wgts/img-gif.js
@@ -19,30 +19,16 @@ export default class WgtImgGif extends Component {
     render() {
 
         const { info } = this.props
+        if(!info){
+            return null
+        }
 
         return (
             <View className={`index ${info.base.padded ? 'wgt__padded' : null}`}>
-                <View className='imglist' style={`background:url(${info.data[0].imgUrl})`}>
-                    {/* <Image
-                        mode='widthFix'
-                        className='scale-placeholder png'
-                        src={info.data[0].imgUrl}
-                    /> */}
-                    {/* <Image
-                        
-                        className='scale-placeholder gif'
-                        src={info.data[1].imgUrl}
-                    /> */}
-                     {/* <SpImg
-                        img-class='scale-placeholder png'
-                        src={info.data[0].imgUrl}
-                        mode='widthFix'
-                        width='750'
-                        lazyLoad
-                      /> */}
+                <View className='imglist' style={`background:url(${info.data && info.data[0].imgUrl})`}>
                      <SpImg
                         img-class='scale-placeholder gif'
-                        src={info.data[1].imgUrl}
+                        src={info.data && info.data[1].imgUrl}
                         mode='widthFix'
                         width='750'
                         lazyLoad
