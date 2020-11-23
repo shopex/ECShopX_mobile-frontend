b3745b588ab7f162a044e73d4562b0bca8bff6eb
[wangtengteng] <[wangtengteng@shopex.cn]>
2020-11-23 18:37:44 +0800

    冲突

--- config/host.js
+++ config/host.js
@@ -5,9 +5,9 @@ module.exports = {
     test: 'ecshopx.shopex123.com/index.php'
   },
   weapp: {
-    prod: 'ecshopx.shopex123.com/index.php',
-    preissue: 'ecshopx.shopex123.com/index.php',
-    test: 'ecshopx.shopex123.com/index.php'
+    prod: 'ecshopx2.shopex123.com',
+    preissue: 'ecshopx2.shopex123.com',
+    test: 'ecshopx2.shopex123.com'
   },
   websocket: {
     development: 'wss://ecshopx.shopex123.com/ws',
