cbc9867cd58d661e7f61077046f201f9d8dde70c
wujiabao <wujiabao@shopex.cn>
2021-02-26 16:57:00 +0800

    fix

--- src/pages/pointitem/list.js
+++ src/pages/pointitem/list.js
@@ -3,17 +3,16 @@ import { View, Text, ScrollView, Image } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
 import { withPager, withBackToTop } from '@/hocs'
 import { AtDrawer,AtInput } from 'taro-ui'
-import { BackToTop, Loading, TagsBar, FilterBar, SearchBar, SpNote, NavBar, TabBar } from '@/components'
+import { BackToTop, Loading,SpNote, NavBar, TabBar } from '@/components'
 import api from '@/api'
 import { Tracker } from "@/service";
-import { pickBy, classNames } from '@/utils'
-import entry from "../../utils/entry";
+import { classNames } from '@/utils' 
 import Header from './comps/header'
 import Tabs from './comps/tabs'
 import GoodsItem from './comps/goods_item'
 import FilterBlock from './comps/filter-block'
 import S from '@/spx'
-import './list.scss'
+import './list.scss' 
 
 @connect(({
   member
@@ -28,28 +27,21 @@ export default class List extends Component {
 
     this.state = {
       ...this.state,
-      curFilterIdx: 0,
-      curTagId: '',
+      //当前筛选菜单下标
+      curFilterIdx: 0, 
+      //筛选菜单
       filterList: [
         { title: '综合' },
         { title: '销量' },
         { title: '价格', sort: -1 },
         { title: '筛选' },
       ],
+      //获取用户信息
       userInfo:{},
       query: null,
-      list: [],
-      oddList: [],
-      evenList: [],
-      tagsList: [],
-      paramsList: [],
-      listType: 'grid',
-      isShowSearch: false,
-      showDrawer: false,
-      selectParams: [],
-      info: {},
-      shareInfo: {},
-      isOpenStore: null,
+      list: [],   
+      showDrawer: false, 
+      info: {}, 
       filterConfig:{
         brandList:[],
         categoryList:[],
@@ -69,42 +61,25 @@ export default class List extends Component {
     // navigationBarBackgroundColor: '#2600b7',
     navigationStyle: 'custom',
     backgroundColor: '#F5F5F5'
-  }
- 
-  async getPointitemConfig(){
-    
-  }
+  } 
 
   async componentDidMount() {
-    const { cat_id = null, main_cat_id = null } = this.$router.params
-    this.firstStatus = true
-    const isOpenStore = await entry.getStoreStatus()
-    const { store_id } = Taro.getStorageSync('curStore')
-    this.setState({
-      isOpenStore
-    })
-    this.fetchUserInfo();
-
-    this.getPointitemConfig();
+    const { keywords,dis_id,cat_id, main_cat_id } = this.$router.params 
+ 
+    this.fetchUserInfo(); 
 
     this.fetchConfig();
 
     this.setState({
       query: {
-        keywords: this.$router.params.keywords,
+        keywords: keywords?keywords:undefined,
         item_type: 'normal', 
-        distributor_id: isOpenStore ? store_id : this.$router.params.dis_id, 
-        category: cat_id ? cat_id : '',
-        main_category: main_cat_id ? main_cat_id : ''
-      },
-      curTagId: this.$router.params.tag_id
+        distributor_id: dis_id?dis_id:undefined, 
+        category: cat_id ? cat_id : undefined,
+        main_category: main_cat_id ? main_cat_id : undefined
+      }, 
     }, () => {
-      this.nextPage()
-      api.wx.shareSetting({ shareindex: 'itemlist' }).then(res => {
-        this.setState({
-          shareInfo: res
-        })
-      })
+      this.nextPage() 
     })
   }
 
@@ -121,32 +96,7 @@ export default class List extends Component {
       })
     }
   }
-
-  onShareAppMessage() {
-    const res = this.state.shareInfo
-    const { cat_id = '', main_cat_id = '' } = this.$router.params
-    const { userId } = Taro.getStorageSync('userinfo')
-    const query = userId ? `?uid=${userId}&cat_id=${cat_id}&main_cat_id=${main_cat_id}` : `?cat_id=${cat_id}&main_cat_id=${main_cat_id}`
-    return {
-      title: res.title,
-      imageUrl: res.imageUrl,
-      path: `/pages/item/list${query}`
-    }
-  }
-
-  onShareTimeline() {
-    const res = this.state.shareInfo
-    const { cat_id = null, main_cat_id = null } = this.$router.params
-    const { userId } = Taro.getStorageSync('userinfo')
-    const query = userId ? `uid=${userId}&cat_id=${cat_id}&main_cat_id=${main_cat_id}` : `cat_id=${cat_id}&main_cat_id=${main_cat_id}`
-    return {
-      title: res.title,
-      imageUrl: res.imageUrl,
-      query: query
-    }
-  }
-
-
+  
 
   async fetchUserInfo(){
     if (!S.getAuthToken()) return 
@@ -228,11 +178,15 @@ export default class List extends Component {
  
     const { page_no: page, page_size: pageSize } = params
 
+    const {filterParams:{brand,category}}=this.state;
+
     const query={
       ...this.state.query,
       page,
       item_type:'normal',
-      pageSize
+      pageSize,
+      brand_id:brand?brand[0]:undefined,
+      main_category:category?category[0]:undefined,
     }
     let total;
     const { list:prevState }=this.state;
@@ -271,102 +225,7 @@ export default class List extends Component {
       total
     }
     
-  }
-
-  // async fetch(params) {
-  //   const { page_no: page, page_size: pageSize } = params
-  //   const { selectParams, tagsList, curTagId, isOpenStore } = this.state
-  //   const { distributor_id, store_id } = Taro.getStorageSync('curStore')
-  //   const { cardId } = this.$router.params
-  //   const query = {
-  //     ...this.state.query,
-  //     item_params: selectParams,
-  //     tag_id: curTagId,
-  //     page,
-  //     pageSize
-  //   }
-
-  //   if (APP_PLATFORM === 'standard') {
-  //     query.distributor_id = isOpenStore ? store_id : distributor_id
-  //   }
-
-  //   if (cardId) {
-  //     query.card_id = cardId
-  //   }
-
-  //   const { list, total_count: total, item_params_list = [], select_tags_list = [] } = await api.item.search(query)
-  //   const { favs } = this.props
-
-  //   item_params_list.map(item => {
-  //     if (selectParams.length < 4) {
-  //       selectParams.push({
-  //         attribute_id: item.attribute_id,
-  //         attribute_value_id: 'all'
-  //       })
-  //     }
-  //     item.attribute_values.unshift({ attribute_value_id: 'all', attribute_value_name: '全部', isChooseParams: true })
-  //   })
-
-  //   const nList = pickBy(list, {
-  //     img: ({ pics }) => pics ? typeof pics !== 'string' ? pics[0] : JSON.parse(pics)[0] : '',
-  //     item_id: 'item_id',
-  //     title: ({ itemName, item_name }) => itemName ? itemName : item_name,
-  //     desc: 'brief',
-  //     distributor_id: 'distributor_id',
-  //     distributor_info: 'distributor_info',
-  //     promotion_activity_tag: 'promotion_activity',
-  //     origincountry_name: 'origincountry_name',
-  //     origincountry_img_url: 'origincountry_img_url',
-  //     type: 'type',
-  //     price: ({ price }) => (price / 100).toFixed(2),
-  //     member_price: ({ member_price }) => (member_price / 100).toFixed(2),
-  //     market_price: ({ market_price }) => (market_price / 100).toFixed(2),
-  //     is_fav: ({ item_id }) => Boolean(favs[item_id])
-  //   })
-
-  //   let odd = [], even = []
-  //   nList.map((item, idx) => {
-  //     if (idx % 2 == 0) {
-  //       odd.push(item)
-  //     } else {
-  //       even.push(item)
-  //     }
-  //   })
-
-  //   this.setState({
-  //     list: [...this.state.list, ...nList],
-  //     oddList: [...this.state.oddList, ...odd],
-  //     evenList: [...this.state.evenList, ...even],
-  //     showDrawer: false,
-  //     query
-  //   }, () => {
-  //     this.startTrack();
-  //   })
-
-  //   if (this.firstStatus) {
-  //     this.setState({
-  //       paramsList: item_params_list,
-  //       selectParams
-  //     })
-  //     this.firstStatus = false
-  //   }
-
-  //   if (tagsList.length === 0) {
-  //     let tags = select_tags_list
-  //     tags.unshift({
-  //       tag_id: 0,
-  //       tag_name: '全部'
-  //     })
-  //     this.setState({
-  //       //curTagId: 0,
-  //       tagsList: tags
-  //     })
-  //   }
-
-  //   return {
-  //     total
-  //   }
-  // }
+  } 
 
   startTrack() {
     this.endTrack();
@@ -401,22 +260,7 @@ export default class List extends Component {
       this.observe = null;
     }
   }
-
-  handleTagChange = (data) => {
-    const { current } = data
-    this.resetPage()
-    this.setState({
-      list: [],
-      oddList: [],
-      evenList: []
-    })
-
-    this.setState({
-      curTagId: current
-    }, () => {
-      this.nextPage()
-    })
-  }
+ 
 
   filterOpen=(flag)=>{
     this.setState({
@@ -457,15 +301,7 @@ export default class List extends Component {
       this.nextPage()
     })
   }
-
-  handleListTypeChange = () => {
-    const listType = this.state.listType === 'grid' ? 'default' : 'grid'
-
-    this.setState({
-      listType
-    })
-  }
-
+ 
   handleClickItem = (item) => {
     const { item_id, title, market_price, price, img } = item;
     Tracker.dispatch("TRIGGER_SKU_COMPONENT", {
@@ -486,176 +322,71 @@ export default class List extends Component {
     Taro.navigateTo({
       url
     })
-  } 
+  }  
 
-  handleClickParmas = (id, child_id) => {
-    const { paramsList, selectParams } = this.state
-    paramsList.map(item => {
-      if (item.attribute_id === id) {
-        item.attribute_values.map(v_item => {
-          if (v_item.attribute_value_id === child_id) {
-            v_item.isChooseParams = true
-          } else {
-            v_item.isChooseParams = false
-          }
-        })
-      }
-    })
-    selectParams.map(item => {
-      if (item.attribute_id === id) {
-        item.attribute_value_id = child_id
-      }
-    })
-    this.setState({
-      paramsList,
-      selectParams
-    })
-  }
-
-  handleClickSearchParams = (type) => {
-  
-    if (type === 'reset') {
-      const { paramsList, selectParams } = this.state
-      this.state.paramsList.map(item => {
-        item.attribute_values.map(v_item => {
-          if (v_item.attribute_value_id === 'all') {
-            v_item.isChooseParams = true
-          } else {
-            v_item.isChooseParams = false
-          }
-        })
-      })
-      selectParams.map(item => {
-        item.attribute_value_id = 'all'
-      })
-      this.setState({
-        paramsList,
-        selectParams
-      })
-    }
-
+  handleClickSearchParams = (type) => { 
     this.resetPage()
-    this.setState({
+    this.setState({ 
+      showDrawer:false,
       list: [],
-      oddList: [],
-      evenList: []
     }, () => {
       this.nextPage()
     })
   }
 
-  handleViewChange = () => {
-    const { listType } = this.state
-    if (listType === 'grid') {
-      this.setState({
-        listType: 'list'
-      })
-    } else {
-      this.setState({
-        listType: 'grid'
-      })
-    }
-  }
-
-  handleSearchOn = () => {
-    console.log("handleSearchOn")
+  handleResetFilter=()=>{
     this.setState({
-      isShowSearch: true
-    })
-  }
-
-  handleSearchOff = () => {
-    this.setState({
-      isShowSearch: false
-    })
-  }
-
-  handleSearchChange = (val) => {
-    this.setState({
-      query: {
-        ...this.state.query,
-        keywords: val
+      filterParams:{
+        brand:[],
+        category:[]
       }
     })
   }
-
-  handleSearchClear = () => {
-    this.setState({
-      isShowSearch: false,
-      query: {
-        ...this.state.query,
-        keywords: ''
-      }
-    }, () => {
-      this.resetPage()
-      this.setState({
-        list: [],
-        oddList: [],
-        evenList: []
-      }, () => {
-        this.nextPage()
-      })
-    })
-  }
-
+    
+   
   handleClickFilterBlock= ({id,type}) =>{
     const { filterParams }=this.state;
     let newFilterParams;
     if(type==='brand'){
+      if(filterParams.brand[0]===id){
+        return ;
+      } 
       newFilterParams={
         ...filterParams,
-        
+        brand:[id], 
+      }
+    }else if(type==='category'){
+      if(filterParams.category[0]===id){
+        return ;
+      } 
+      newFilterParams={
+        ...filterParams,
+        category:[id], 
       }
     }
-  }
-
-  handleConfirm = (val) => {
-
-    Tracker.dispatch("SEARCH_RESULT", {
-      keywords: val
-    });
     this.setState({
-      isShowSearch: false,
-      query: {
-        ...this.state.query,
-        keywords: val,
-      }
-    }, () => {
-      this.resetPage()
-      this.setState({
-        list: [],
-        oddList: [],
-        evenList: []
-      }, () => {
-        this.nextPage()
-      })
+      filterParams:newFilterParams
     })
-  }
-
+  } 
+   
   render() {
     const {
-      list,
-      oddList,
-      evenList,
-      listType,
+      list, 
       curFilterIdx,
       filterList,
       showBackToTop,
       scrollTop,
       page,
-      showDrawer,
-      paramsList,
-      selectParams,
-      tagsList,
-      curTagId,
-      info,
-      isShowSearch,
-      query,
+      showDrawer,  
       useInfo,
       filterConfig:{
         brandList,
         categoryList,
         scoreInternel
+      },
+      filterParams:{
+        brand,
+        category
       }
     } = this.state
     const { isTabBar = '' } = this.$router.params
@@ -691,15 +422,13 @@ export default class List extends Component {
 
           </Tabs>
           <ScrollView
-            className={classNames(isTabBar ? 'goods-list__scroll_isTabBar' : 'goods-list__scroll', tagsList.length > 0 && 'with-tag-bar', isTabBar && 'isTabBar')}
+            className={classNames(isTabBar ? 'goods-list__scroll_isTabBar' : 'goods-list__scroll', isTabBar && 'isTabBar')}
             scrollY
             scrollTop={scrollTop}
             scrollWithAnimation
             onScroll={this.handleScroll}
             onScrollToLower={this.nextPage}
-          >
-            {
-              listType === 'grid' &&
+          > 
               <View className='goods-list goods-list__type-grid'> 
                   {
                     list.map(item => {
@@ -720,9 +449,7 @@ export default class List extends Component {
                     })
                   } 
                
-              </View>
-            }
-
+              </View> 
             {
               page.isLoading
                 ? <Loading>正在加载...</Loading>
@@ -733,35 +460,7 @@ export default class List extends Component {
             }
           </ScrollView>
         </View>
-        {/* <View className='goods-list__toolbar'>
-          <View className={`goods-list__search ${(query && query.keywords && !isShowSearch) ? 'on-search' : null}`}>
-            <SearchBar
-              keyword={query ? query.keywords : ''}
-              onFocus={this.handleSearchOn}
-              onChange={this.handleSearchChange}
-              onClear={this.handleSearchClear}
-              onCancel={this.handleSearchOff}
-              onConfirm={this.handleConfirm.bind(this)}
-            />
-            {
-              !isShowSearch &&
-              <View
-                className={classNames('goods-list__type', listType === 'grid' ? 'icon-list' : 'icon-grid')}
-                onClick={this.handleViewChange}
-              >
-              </View>
-            }
-          </View>
-          {
-            tagsList.length &&
-            <TagsBar
-              current={curTagId}
-              list={tagsList}
-              onChange={this.handleTagChange.bind(this)}
-            />
-          }
-          
-        </View> */}
+        
 
         <AtDrawer
           show={showDrawer}
@@ -779,7 +478,7 @@ export default class List extends Component {
                   {
                     brandList.map((item,index)=>{
                       return (
-                        <FilterBlock info={item} type="brand" onClickItem={this.handleClickFilterBlock} />
+                        <FilterBlock info={item} type="brand" active={brand.indexOf(item.attribute_id)>-1} onClickItem={this.handleClickFilterBlock} />
                       )
                     })
                   }
@@ -791,7 +490,7 @@ export default class List extends Component {
                   {
                     categoryList.map((item,index)=>{
                       return (
-                        <FilterBlock info={item} type="category" onClickItem={this.handleClickFilterBlock} />
+                        <FilterBlock info={item} type="category" active={category.indexOf(item.category_id)>-1} onClickItem={this.handleClickFilterBlock} />
                       )
                     })
                   }
@@ -810,39 +509,9 @@ export default class List extends Component {
               </View>
             </View>
           }
-          {/* {
-            paramsList.map((item, index) => {
-              return (
-                <View className='drawer-item' key={`${index}1`}>
-                  <View className='drawer-item__title'>
-                    <Text>{item.attribute_name}</Text>
-                    <View className='at-icon at-icon-chevron-down'> </View>
-                  </View>
-                  <View className='drawer-item__options'>
-                    {
-                      item.attribute_values.map((v_item, v_index) => {
-                        return (
-                          <View
-                            className={classNames('drawer-item__options__item', v_item.isChooseParams ? 'drawer-item__options__checked' : '')}
-                            // className='drawer-item__options__item'
-                            key={`${v_index}1`}
-                            onClick={this.handleClickParmas.bind(this, item.attribute_id, v_item.attribute_value_id)}
-                          >
-                            {v_item.attribute_value_name}
-                          </View>
-                        )
-                      })
-                    }
-                    <View className='drawer-item__options__none'> </View>
-                    <View className='drawer-item__options__none'> </View>
-                    <View className='drawer-item__options__none'> </View>
-                  </View>
-                </View>
-              )
-            })
-          } */}
+          
           <View className='drawer-footer'>
-            <Text className='drawer-footer__btn' onClick={this.handleClickSearchParams.bind(this, 'reset')}>重置</Text>
+            <Text className='drawer-footer__btn' onClick={this.handleResetFilter}>重置</Text>
             <Text className='drawer-footer__btn drawer-footer__btn_active' onClick={this.handleClickSearchParams.bind(this, 'submit')}>确定并筛选</Text>
           </View>
         </AtDrawer>
