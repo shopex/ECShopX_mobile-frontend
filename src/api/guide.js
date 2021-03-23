e74fad106a91be66d3ee1f69302ac3d2ef456e03
wangzhanyuan <wangzhanyuan@shopex.cn>
2021-03-23 15:02:56 +0800

    推进导购货架进度

--- src/api/guide.js
+++ src/api/guide.js
@@ -1,40 +1,48 @@
-import req from './req'
+import req from "./req";
 import S from "@/spx";
-console.log('---req-guide-S---',S)
-function createHead (){
-  console.log('---req-guide-S-createHead---',S)
-  return {header: {
-    "x-wxapp-session": S&&S.get('session3rd',true)||'',
-    "salesperson-type": "shopping_guide"
-  }}
+console.log("---req-guide-S---", S);
+function createHead() {
+  console.log("---req-guide-S-createHead---", S);
+  return {
+    header: {
+      "x-wxapp-session": (S && S.get("session3rd")) || "",
+      "salesperson-type": "shopping_guide"
+    }
+  };
 }
- 
+
 //获取导购店铺列表
-export function distributorlist (params = {}) {
-  return req.get('/salesperson/distributorlist', params)
+export function distributorlist(params = {}) {
+  return req.get("/salesperson/distributorlist", params);
 }
 //验证导购员的店铺id是否有效
-export function is_valid (params = {},config=createHead()) {
-  console.log('/salesperson/distributor/is_valid')
-  return req.get('/salesperson/distributor/is_valid', params,config)
+export function is_valid(params = {}, config = createHead()) {
+  console.log("/salesperson/distributor/is_valid");
+  return req.get("/salesperson/distributor/is_valid", params, config);
 }
 //扫条形码加入购物车
-export function scancodeAddcart (params = {}) {
-  return req.post('/salesperson/scancodeAddcart', params)
+export function scancodeAddcart(params = {}, config = createHead()) {
+  return req.post("/salesperson/scancodeAddcart", params, config);
 }
 //导购员购物车新增
-export function cartdataadd (params = {}) {
-  return req.get('/salesperson/cartdataadd', params)
+export function cartdataadd(params = {}, config = createHead()) {
+  return req.get("/salesperson/cartdataadd", params, config);
 }
 //导购员购物车更新
-export function cartdataupdate (params = {}) {
-  return req.get('/salesperson/cartdataupdate', params)
+export function cartdataupdate(params = {}, config = createHead()) {
+  return req.get("/salesperson/cartdataupdate", params, config);
 }
 //获取导购员购物车
-export function cartdatalist (params = {}) {
-  return req.get('/salesperson/cartdatalist', params)
+export function cartdatalist(params = {}, config = createHead()) {
+  return req.get("/salesperson/cartdatalist", params, config);
 }
 //导购员购物车删除
-export function cartdatadel (params = {}) {
-  return req.get('/salesperson/cartdatadel', params)
+export function cartdatadel(params = {}, config = createHead()) {
+  return req.get("/salesperson/cartdatadel", params, config);
 }
+
+//生成海报的二维码，增加一个参数 salesman_distributor_id:导购的店铺id
+export function salesPromotion(params = {}, config = createHead()) {
+  return req.get("/salesperson/salesPromotion", params, config);
+}
+ 
