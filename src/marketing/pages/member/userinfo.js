fe9f812d2dafb194b5994900b17b7aa07eb787fc
liuhongxu <liuhongxu@shopex.cn>
2021-04-28 13:23:54 +0800

    feat: 个人信心页调整

--- src/marketing/pages/member/userinfo.js
+++ src/marketing/pages/member/userinfo.js
@@ -1,12 +1,12 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text, Picker } from '@tarojs/components'
 import {AtButton, AtForm, AtInput, AtAvatar} from 'taro-ui'
-import { NavBar, SpToast, SpCheckbox } from '@/components'
+import { NavBar, SpCheckbox } from '@/components'
 import api from '@/api'
 // import { withLogin } from '@/hocs'
 import S from '@/spx'
 import azureUploader from '@/utils/azure-wry'
-import { classNames, isArray, isString } from '@/utils'
+import { isArray, isString } from '@/utils'
 import { connect } from "@tarojs/redux"
 
 import './userinfo.scss'
@@ -33,6 +33,10 @@ export default class UserInfo extends Component {
 
   componentDidMount () {
     // this.fetch()
+  }
+
+  config = {
+    navigationBarTitleText: '个人信息'
   }
 
   async fetch () {
@@ -102,7 +106,9 @@ export default class UserInfo extends Component {
     }
 
     if (data.length > 1) {
-      S.toast('最多上传1张图片')
+      Taro.showToast({
+        title: '最多上传1张图片'
+      })
     }
     const imgFiles = data.slice(0, 1)
     azureUploader.uploadImagesFn(imgFiles)
@@ -165,7 +171,9 @@ export default class UserInfo extends Component {
     }
     try {
       await api.member.setMemberInfo(data)
-      S.toast('修改成功')
+      Taro.showToast({
+        title: '修改成功'
+      })
       setTimeout(()=>{
         Taro.redirectTo({
           url: '/pages/member/index'
@@ -254,143 +262,16 @@ export default class UserInfo extends Component {
 
 
   render () {
-    const { isHasAvator, info, imgs, isHasData, list, option_list, showCheckboxPanel } = this.state
-    const { colors } = this.props
+    // const { isHasAvator, info, imgs, isHasData, list, option_list, showCheckboxPanel } = this.state
+    // const { colors } = this.props
     return (
       <View className='page-member-setting'>
         <NavBar
           title='用户信息'
         />
-
-        <AtForm
-          onSubmit={this.handleSubmit}
-        >
-          <View className='sec auth-login__form'>
-            <View className='avatar-user'>
-              <View className='avatar-user-text'>头像</View>
-              {/* <AtImagePicker
-                showAddBtn={isHasAvator}
-                mode='aspectFill'
-                length={1}
-                files={imgs}
-                onChange={this.handleImageChange}
-                onImageClick={this.handleImageClick}
-              > </AtImagePicker> */}
-              <AtAvatar mode='aspectFill' image={info.avatar}></AtAvatar>
-            </View>
-            <AtInput
-              title='昵称'
-              value={info.user_name}
-              placeholder='请输入昵称'
-              editable={false}
-              onFocus={this.handleErrorToastClose}
-              onChange={this.handleChange.bind(this, 'user_name')}
-            />
-            {
-              isHasData && list.map((item, index) => {
-                return (
-                  <View key={`${index}1`}>
-                    {
-                      item.element_type === 'input'
-                        ? <View key={`${index}1`}>
-                            <AtInput
-                              key={`${index}1`}
-                              title={item.name}
-                              name={`${item.key}`}
-                              placeholder={`请输入${item.name}`}
-                              value={item.value}
-                              onFocus={this.handleErrorToastClose}
-                              onChange={this.handleChange.bind(this, `${item.key}`)}
-                              ref={(input) => { this.textInput = input }}
-                            />
-                          </View>
-                        : null
-                    }
-                    {
-                      item.element_type === 'select'
-                        ? <View className='page-section'>
-                            <View key={`${index}1`}>
-                              {
-                                item.key === 'birthday'
-                                  ? <Picker mode='date' onChange={this.handleChange.bind(this, `${item.key}`)}>
-                                    <View className='picker'>
-                                      <View className='picker__title'>{item.name}</View>
-                                      <Text
-                                        className={classNames(item.value ? 'pick-value' : 'pick-value-null')}
-                                      >{item.value ? item.value : `请选择${item.name}`}</Text>
-                                    </View>
-                                  </Picker>
-                                  : <Picker mode='selector' range={item.items} key={`${index}1`} data-item={item} onChange={this.handleChange.bind(this, `${item.key}`)}>
-                                    <View className='picker'>
-                                      <View className='picker__title'>{item.name}</View>
-                                      <Text
-                                        className={classNames(item.value ? 'pick-value' : 'pick-value-null')}
-                                      >{item.value ? item.value : `请选择${item.name}`}</Text>
-                                    </View>
-                                  </Picker>
-                              }
-                            </View>
-                          </View>
-                        : null
-                    }
-                    {
-                      item.element_type === 'checkbox'
-                        ? <View className='page-section'>
-                            <AtInput
-                              key={`${index}1`}
-                              title={item.name}
-                              name={`${item.key}`}
-                              placeholder={`请选择${item.name}`}
-                              value={item.value}
-                              onFocus={this.showCheckboxPanel.bind(this, item.items, item.key)}
-                            />
-                          </View>
-                        : null
-                    }
-                  </View>
-                )
-              })
-            }
-          </View>
-          <View className='btns'>
-            {
-              process.env.TARO_ENV === 'weapp'
-                ? <AtButton type='primary' formType='submit'>保存</AtButton>
-                : <AtButton type='primary' onClick={this.handleSubmit} formType='submit'>保存</AtButton>
-            }
-          </View>
-        </AtForm>
-        {
-          showCheckboxPanel
-            ? <View className='checkBoxPanel'>
-                <View className='checkBoxPanel-content'>
-                  {
-                    option_list.map((item, index) => {
-                      return (
-                        <View
-                          className='checkBoxPanel-item'
-                          key={`${index}1`}
-                        >
-                          <SpCheckbox
-                            checked={item.ischecked}
-                            onChange={this.handleSelectionChange.bind(this, item.name)}
-                          >{item.name}</SpCheckbox>
-                        </View>
-                      )
-                    })
-                  }
-                </View>
-                <View className='panel-btns'>
-                  <View className='panel-btn cancel-btn' onClick={this.btnClick.bind(this, 'cancel')}>取消</View>
-                  <View
-                    className='panel-btn require-btn'
-                    style={`background: ${colors.data[0].primary}`}
-                    onClick={this.btnClick.bind(this, 'require')}>确定</View>
-                </View>
-              </View>
-          : null
-        }
-        <SpToast />
+        <View className='btns'>
+          按钮组
+        </View>
       </View>
     )
   }
