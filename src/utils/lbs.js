01efcdc45a88ae94409b7f86ac3075bd07b2d9e4
lukaijie <lukaijie@shopex.cn>
2021-08-26 19:41:39 +0800

    定位

--- src/utils/lbs.js
+++ src/utils/lbs.js
@@ -9,7 +9,9 @@ class LBS {
   loadJScript() {
     const script = document.createElement("script")
     script.type = "text/javascript"
-    script.src = `https://apis.map.qq.com/tools/geolocation/min?key=${process.env.APP_MAP_KEY}&referer=${process.env.APP_MAP_NAME}`
+    // script.src = `https://apis.map.qq.com/tools/geolocation/min?key=${process.env.APP_MAP_KEY}&referer=${process.env.APP_MAP_NAME}`
+    // script.src = `https://map.qq.com/api/gljs?v=1.exp&key=${process.env.APP_MAP_KEY}`;
+    script.src = `https://mapapi.qq.com/web/mapComponents/geoLocation/v/geolocation.min.js`;
     document.body.appendChild(script)
   }
 }
