00754a1312ced73815c4992bc2dd340c2bceb290
WJB3 <2459666474@qq.com>
2021-09-08 13:33:01 +0800

    fix

--- src/components/sp-float-privacy/index.js
+++ src/components/sp-float-privacy/index.js
@@ -81,7 +81,7 @@ export default class SpFloatPrivacy extends Component {
         fail: (res) => {
         },
         success: async (res) => {
-          let userInfo = JSON.parse(res.response).response ;
+          let userInfo = JSON.parse(res.response).response;
           await api.member.updateMemberInfo({
             username: userInfo.nickName,
             avatar: userInfo.avatar
@@ -111,11 +111,41 @@ export default class SpFloatPrivacy extends Component {
       >
         <View className="sp-float-privacy__overlay"></View>
         <View className="sp-float-privacy__wrap">
-          <View className="privacy-hd">个人隐私保护指引</View>
-          <View className="privacy-bd">
+          <View className="privacy-hd">个人隐私保护1指引</View>
+
+          {isWeixin && <View className="privacy-bd">
+            请您务必审慎阅读、充分理解“服务协议”和“隐私政策”各条款，包括但不限于：为了向您提供更好的服务，我们须向您收集设备信息、操作日志等个人信息。您可以在“设置”中查看、变更、删除个人授权信息。
+            您可阅读
+            <Text
+              className="privacy-txt"
+              onClick={this.navigateTo.bind(
+                this,
+                "/subpage/pages/auth/reg-rule?type=member_register"
+              )}
+            >
+              《{info.protocol.member_register}》
+            </Text>
+            、
+            <Text
+              className="privacy-txt"
+              onClick={this.navigateTo.bind(
+                this,
+                "/subpage/pages/auth/reg-rule?type=privacy"
+              )}
+            >
+              《{info.protocol.privacy}》
+            </Text>
+            了解详细信息。如您同意，请点击“同意”开始接受我们的服务。
+
             您可以在“设置”中查看、变更、删除个人授权信息。
             如您同意，请点击“同意”开始接受我们的服务。
-          </View>
+          </View>}
+
+          {isAlipay && <View className="privacy-bd">
+            您可以在“设置”中查看、变更、删除个人授权信息。
+            如您同意，请点击“同意”开始接受我们的服务。
+          </View>}
+          
           <View className="privacy-ft">
             <View className="btn-wrap">
               <AtButton onClick={this.handleCancel.bind(this)}>不同意</AtButton>
--- src/marketing/pages/member/edit-address.js
+++ src/marketing/pages/member/edit-address.js
@@ -4,20 +4,20 @@ import { View, Switch, Text, Picker, Button } from '@tarojs/components'
 import { AtForm, AtInput } from 'taro-ui'
 import { connect } from '@tarojs/redux'
 import { SpCell, SpToast, NavBar } from '@/components'
-import api from '@/api'
-import { pickBy,isAlipay,showLoading,hideLoading } from '@/utils'
+import api from '@/api' 
+import { pickBy,isWeixin, isAlipay, showLoading, hideLoading } from '@/utils'
 import S from '@/spx'
 
-import './edit-address.scss'
+import './edit-address.scss' 
 
 //转换属性
-const traverseData=(data)=>{
-  let item=[];
-  data.forEach(d=>{
-    let newData={};
-    newData.name=d.label;
-    if(d.children){
-      newData.subList=traverseData(d.children);
+const traverseData = (data) => {
+  let item = [];
+  data.forEach(d => {
+    let newData = {};
+    newData.name = d.label;
+    if (d.children) {
+      newData.subList = traverseData(d.children);
     }
     item.push(newData)
   })
@@ -27,8 +27,9 @@ const traverseData=(data)=>{
 @connect(({ colors }) => ({
   colors: colors.current
 }))
+
 export default class AddressIndex extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
 
     this.state = {
@@ -36,17 +37,17 @@ export default class AddressIndex extends Component {
       areaList: [],
       multiIndex: [],
       listLength: 0,
-      areaListAli:[],
-      selectedListAli:[]
+      areaListAli: [],
+      selectedListAli: []
       // ubmitLoading: false,
     }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.fetch()
   }
 
-  async fetch () {
+  async fetch() {
     showLoading()
     const { list } = await api.member.addressList()
     this.setState({
@@ -54,62 +55,70 @@ export default class AddressIndex extends Component {
     })
 
     list.map(a_item => {
-      if(a_item.address_id === this.$router.params.address_id) {
+      if (a_item.address_id === this.$router.params.address_id) {
         this.setState({
           info: a_item
         })
       }
     })
 
-    let res = await api.member.areaList();  
+    let res = await api.member.areaList();
 
-    this.setState({
-      areaListAli:traverseData(res)
-    }) 
+    if (isAlipay) {
+      this.setState({
+        areaListAli: traverseData(res)
+      })
+    }
 
-    // const nList = pickBy(res, {
-    //   name: 'label',
-    //   subList: 'children',
-    // })
-    // console.log("---api.member.areaList2---",nList);
-    // this.nList = nList
-    // let arrProvice = []
-    // let arrCity = []
-    // let arrCounty = []
-    // nList.map((item, index) => {
-    //   arrProvice.push(item.label)
-    //   if(index === 0) {
-    //     item.children.map((c_item, c_index) => {
-    //       arrCity.push(c_item.label)
-    //       if(c_index === 0) {
-    //         c_item.children.map(cny_item => {
-    //           arrCounty.push(cny_item.label)
-    //         })
-    //       }
-    //     })
-    //   }
-    // })
-    // this.setState({
-    //   areaList: [arrProvice, arrCity, arrCounty],
-    //   // areaList: [['北京'], ['北京'], ['东城']],
-    // })
+    if (isWeixin) {
+      const nList = pickBy(res, {
+        label: 'label',
+        children: 'children',
+      })
+      console.log("---api.member.areaList2---", nList);
+      this.nList = nList
+      let arrProvice = []
+      let arrCity = []
+      let arrCounty = []
+      nList.map((item, index) => {
+        arrProvice.push(item.label)
+        if (index === 0) {
+          item.children.map((c_item, c_index) => {
+            arrCity.push(c_item.label)
+            if (c_index === 0) {
+              c_item.children.map(cny_item => {
+                arrCounty.push(cny_item.label)
+              })
+            }
+          })
+        }
+      })
+      this.setState({
+        areaList: [arrProvice, arrCity, arrCounty],
+        // areaList: [['北京'], ['北京'], ['东城']],
+      })
 
-    // if (this.$router.params.isWechatAddress){
-    //   const resAddress = await Taro.chooseAddress()
-    //   const query = {
-    //     province: resAddress.provinceName,
-    //     city: resAddress.cityName,
-    //     county: resAddress.countyName,
-    //     adrdetail: resAddress.detailInfo,
-    //     is_def: 0,
-    //     postalCode: resAddress.postalCode,
-    //     telephone: resAddress.telNumber,
-    //     username: resAddress.userName,
-    //   }
-    //   this.setState({
-    //     info: query
-    //   })
-    // }
+      if (this.$router.params.isWechatAddress) {
+        const resAddress = await Taro.chooseAddress()
+        const query = {
+          province: resAddress.provinceName,
+          city: resAddress.cityName,
+          county: resAddress.countyName,
+          adrdetail: resAddress.detailInfo,
+          is_def: 0,
+          postalCode: resAddress.postalCode,
+          telephone: resAddress.telNumber,
+          username: resAddress.userName,
+        }
+        this.setState({
+          info: query
+        })
+      }
+    }
+
+
+
+
     hideLoading()
   }
 
@@ -118,13 +127,13 @@ export default class AddressIndex extends Component {
     let arrProvice = []
     let arrCity = []
     let arrCounty = []
-    if(this.nList){
+    if (this.nList) {
       this.nList.map((item, index) => {
         arrProvice.push(item.label)
-        if(index === 0) {
+        if (index === 0) {
           item.children.map((c_item, c_index) => {
             arrCity.push(c_item.label)
-            if(c_index === 0) {
+            if (c_index === 0) {
               c_item.children.map(cny_item => {
                 arrCounty.push(cny_item.label)
               })
@@ -143,13 +152,13 @@ export default class AddressIndex extends Component {
   bindMultiPickerChange = async (e) => {
     const { info } = this.state
     this.nList.map((item, index) => {
-      if(index === e.detail.value[0]) {
+      if (index === e.detail.value[0]) {
         info.province = item.label
-        item.children.map((s_item,sIndex) => {
-          if(sIndex === e.detail.value[1]) {
+        item.children.map((s_item, sIndex) => {
+          if (sIndex === e.detail.value[1]) {
             info.city = s_item.label
-            s_item.children.map((th_item,thIndex) => {
-              if(thIndex === e.detail.value[2]) {
+            s_item.children.map((th_item, thIndex) => {
+              if (thIndex === e.detail.value[2]) {
                 info.county = th_item.label
               }
             })
@@ -162,17 +171,17 @@ export default class AddressIndex extends Component {
 
   bindMultiPickerColumnChange = (e) => {
     const { areaList, multiIndex } = this.state
-    if(e.detail.column === 0) {
+    if (e.detail.column === 0) {
       this.setState({
-        multiIndex: [e.detail.value,0,0]
+        multiIndex: [e.detail.value, 0, 0]
       })
       this.nList.map((item, index) => {
-        if(index === e.detail.value) {
+        if (index === e.detail.value) {
           let arrCity = []
           let arrCounty = []
           item.children.map((c_item, c_index) => {
             arrCity.push(c_item.label)
-            if(c_index === 0) {
+            if (c_index === 0) {
               c_item.children.map(cny_item => {
                 arrCounty.push(cny_item.label)
               })
@@ -188,9 +197,9 @@ export default class AddressIndex extends Component {
       multiIndex[2] = 0
       this.setState({
         multiIndex
-      },()=>{
-        this.nList[multiIndex[0]].children.map((c_item, c_index)  => {
-          if(c_index === e.detail.value) {
+      }, () => {
+        this.nList[multiIndex[0]].children.map((c_item, c_index) => {
+          if (c_index === e.detail.value) {
             let arrCounty = []
             c_item.children.map(cny_item => {
               arrCounty.push(cny_item.label)
@@ -227,7 +236,7 @@ export default class AddressIndex extends Component {
   }
 
   handleSubmit = async (e) => {
-    const { value } = e.detail||{}
+    const { value } = e.detail || {}
     const { selectedListAli } = this.state
     const data = {
       ...this.state.info,
@@ -239,7 +248,7 @@ export default class AddressIndex extends Component {
     } else {
       data.is_def = '1'
     }
-    if(this.state.listLength === 0) {
+    if (this.state.listLength === 0) {
       data.is_def = '1'
     }
 
@@ -253,69 +262,63 @@ export default class AddressIndex extends Component {
 
     if (!data.province) {
       data.province = selectedListAli[0]
-      data.city =  selectedListAli[1]
-      data.county =  selectedListAli[2]
+      data.city = selectedListAli[1]
+      data.county = selectedListAli[2]
     }
 
     if (!data.adrdetail) {
       return S.toast('请输入详细地址')
     }
 
-      // if(isAlipay){ 
-      //   data.city='上海市';
-      //   data.province='上海市';
-      //   data.postalCode=100010;
-      //   data.county='徐汇';
-      // }
-    console.log(data)
     showLoading({
       title: '正在提交',
       mask: true
     })
- 
+
     try {
       await api.member.addressCreateOrUpdate(data)
-      if(data.address_id) {
+      if (data.address_id) {
         S.toast('修改成功')
       } else {
         S.toast('创建成功')
       }
-      setTimeout(()=>{
+      setTimeout(() => {
         Taro.navigateBack()
       }, 700)
-    } catch (error) { 
+    } catch (error) {
       hideLoading()
       return false
-    } 
+    }
     hideLoading()
   }
 
-  handleSelectArea=()=>{
-    const { areaListAli } =this.state;
-    console.log("--areaList--",areaListAli)
+  handleSelectArea = () => {
+    const { areaListAli } = this.state;
+    console.log("--areaList--", areaListAli)
     my.multiLevelSelect({
-      title:'请选择省市县区域',
-      list:areaListAli,
-      success:(res)=>{
-        console.log("----handleSelectArea---",res.result)
+      title: '请选择省市县区域',
+      list: areaListAli,
+      success: (res) => {
+        console.log("----handleSelectArea---", res.result)
         this.setState({
-          selectedListAli:[res.result[0].name,res.result[1].name,res.result[2].name]
-        }) 
+          selectedListAli: [res.result[0].name, res.result[1].name, res.result[2].name]
+        })
       }
     })
   }
 
-  render () {
+  render() {
     const { colors } = this.props
     const { info, multiIndex, selectedListAli } = this.state
 
     return (
       <View className='page-address-edit'>
+
         {/*<EditAddress*/}
-          {/*address={this.$router.params.address}*/}
-          {/*addressID={this.$router.params.address_id}*/}
+        {/*address={this.$router.params.address}*/}
+        {/*addressID={this.$router.params.address_id}*/}
         {/*/>*/}
-        <NavBar 
+        <NavBar
           title='编辑地址'
           leftIconType='chevron-left'
           fixed='true'
@@ -337,32 +340,40 @@ export default class AddressIndex extends Component {
               value={info.telephone}
               onChange={this.handleChange.bind(this, 'telephone')}
             />
-            {/* <Picker
-              mode='multiSelector'
-              onClick={this.handleClickPicker}
-              onChange={this.bindMultiPickerChange}
-              onColumnChange={this.bindMultiPickerColumnChange}
-              value={multiIndex}
-              range={areaList}
-            > */}
-              <View className='picker' onClick={this.handleSelectArea}>
+
+            {
+              isAlipay && <View className='picker' onClick={this.handleSelectArea}>
                 <View className='picker__title' >所在区域</View>
                 {
                   selectedListAli.length && <Text>{`${selectedListAli[0]}${selectedListAli[1]}${selectedListAli[2]}`}</Text>
-                }
-                {/* {
-                  info.address_id || (this.$router.params.isWechatAddress && info.province)
-                    ? `${info.province}${info.city}${info.county}`
-                    : <View>
-                      {
-                        multiIndex.length > 0
-                          ? <Text>{areaList[0][multiIndex[0]]}{areaList[1][multiIndex[1]]}{areaList[2][multiIndex[2]]}</Text>
-                          : null
-                      }
-                    </View>
-                } */}
-              </View>
-            {/* </Picker> */}
+                }</View>
+            }
+
+            {
+              isWeixin && <Picker
+                mode='multiSelector'
+                onClick={this.handleClickPicker}
+                onChange={this.bindMultiPickerChange}
+                onColumnChange={this.bindMultiPickerColumnChange}
+                value={multiIndex}
+                range={areaList}
+              >
+                <View className='picker'>
+                  <View className='picker__title'>所在区域</View>
+                  {
+                    info.address_id || (this.$router.params.isWechatAddress && info.province)
+                      ? `${info.province}${info.city}${info.county}`
+                      : <View>
+                        {
+                          multiIndex.length > 0
+                            ? <Text>{areaList[0][multiIndex[0]]}{areaList[1][multiIndex[1]]}{areaList[2][multiIndex[2]]}</Text>
+                            : null
+                        }
+                      </View>
+                  }
+                </View>
+              </Picker>
+            }
 
             <AtInput
               title='详细地址'
