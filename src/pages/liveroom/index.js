7deeea826dfd2073f1f266c63071ce67def6e75c
maqian <maqian@shopex.cn>
2021-09-26 18:32:38 +0800

    调整列表界面

--- src/pages/liveroom/index.js
+++ src/pages/liveroom/index.js
@@ -1,7 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Image, ScrollView, Text } from '@tarojs/components'
 import { AtCountdown } from "taro-ui"
-import { debounce, calcTimer } from '@/utils'
+import { debounce, calcTimer, classNames } from '@/utils'
 import api from '@/api'
 import LoadingMore from '@/boost/component/loadingMore'
 
@@ -110,7 +110,7 @@ export default class LiveRoomList extends Component {
         onRefresherRefresh={this.handleRefresh}
         onScrollToLower={this.handleLoadMore}
       >
-        <View className='liveroom-page-box'>
+        {/* <View className='liveroom-page-box'>
           <View className='liveroom-page-left'>
             <Image className='left-img' mode='widthFix' src={liveRoomList[0].cover_img}></Image>
             <View className='left-state'>
@@ -167,7 +167,7 @@ export default class LiveRoomList extends Component {
             <View className='right-count'>
               <View className='fs'>直播时长：</View>
               <View className='fs'>4小时16分05秒</View>
-              </View>
+            </View>
             <View className='right-btn return return-border'><Image className='icons' mode='aspectFit' src={`${APP_IMAGE_CDN}/return_b.png`} />查看回放</View>
           </View>
         </View>
@@ -184,52 +184,97 @@ export default class LiveRoomList extends Component {
           <View className='liveroom-page-right'>
             <View className='right-title'>科技布沙发现代轻奢直播名称最多显示两行…</View>
           </View>
-        </View>
-        {/* {liveRoomList.map(item => (
-          <View style={{ position: 'relative' }} onClick={this.onLocation.bind(this, item)} key={item.roomid}>
-            <Image className='liveroom-page-bck' mode='aspectFit' src={item.cover_img}></Image>
-            <View className='liveroom-page-title'>{item.name}</View>
-            {
-              item.live_status == 101 &&
-              <View className='liveroom-page-state'>
-                <View className='live'>直播</View>
-                <View className='content'>正在直播中</View>
-              </View>
-            }
-            {
-              item.live_status == 102 &&
-              <View className='liveroom-page-state'>
-                <View className='live notice'>预告</View>
-                <View className='content'>
-                  <AtCountdown
-                    isShowDay={this.timeStamp(item.start_time).dd != 0}
-                    isShowHour={this.timeStamp(item.start_time).hh != 0}
-                    format={{ day: '天', hours: '时', minutes: '分', seconds: '' }}
-                    day={this.timeStamp(item.start_time).dd}
-                    hours={this.timeStamp(item.start_time).hh}
-                    minutes={this.timeStamp(item.start_time).mm}
-                    seconds={this.timeStamp(item.start_time).ss}
-                  />
+        </View> */}
+        {liveRoomList.map(item => {
+          let liveing = item.live_status == 101 // 直播中
+          let notStarted = item.live_status == 102 // 未开始（预告）
+          let playback = item.live_status == 103 && item.close_replay == 0 // 回放
+          let other = (item.live_status == 103 && item.close_replay == 1) || item.live_status == 104 || item.live_status == 105 || item.live_status == 106 || item.live_status == 107
+          return (
+            <View className='liveroom-page-box' key={item.roomid}>
+              <View className='liveroom-page-left'>
+                <Image className='left-img' mode='widthFix' src={item.cover_img} />
+                <View className='left-state'>
+                  <View className={classNames('lives', notStarted && 'notice' || (other || playback) && 'return')}>
+                    {
+                      liveing &&
+                      <Image className='icons' mode='aspectFit' src={`${APP_IMAGE_CDN}/live_w.png`} />
+                    }
+                    {
+                      notStarted &&
+                      <Image className='icons' mode='aspectFit' src={`${APP_IMAGE_CDN}/notice_w.png`} />
+                    }
+                    {
+                      playback &&
+                      <Image className='icons' mode='aspectFit' src={`${APP_IMAGE_CDN}/return_b.png`} />
+                    }
+                    {
+                      other &&
+                      <Image className='icons' mode='aspectFit' src={`${APP_IMAGE_CDN}/over_b.png`} />
+                    }
+                  </View>
+                  <View className='content'>
+                    {
+                      liveing && '正在直播中' ||
+                      notStarted && '预告' ||
+                      playback && '回放' ||
+                      other && '已结束'
+                    }
+                  </View>
                 </View>
               </View>
-            }
-            {
-              item.live_status == 103 && item.close_replay == 0 &&
-              <View className='liveroom-page-state'>
-                <View className='live return'>回放</View>
-                <View className='content'>{item.live_time_text}</View>
+              <View className='liveroom-page-right'>
+                <View className='right-title'>{item.name}</View>
+                {/* 直播时间 */}
+                {
+                  notStarted &&
+                  <View className='right-count'>
+                    <Text>直播时间：</Text>
+                    <AtCountdown
+                      isShowDay={this.timeStamp(1632991411).dd != 0}
+                      isShowHour={this.timeStamp(1632991411).hh != 0}
+                      format={{ day: '天', hours: '时', minutes: '分', seconds: '' }}
+                      day={this.timeStamp(1632991411).dd}
+                      hours={this.timeStamp(1632991411).hh}
+                      minutes={this.timeStamp(1632991411).mm}
+                      seconds={this.timeStamp(1632991411).ss}
+                    />
+                  </View>
+                }
+                {
+                  playback &&
+                  <View className='right-count'>
+                    <View className='fs'>直播时长：</View>
+                    <View className='fs'>{item.live_time_text}</View>
+                  </View>
+                }
+                {/* 右侧按钮 */}
+                {
+                  liveing &&
+                  <View onClick={this.onLocation.bind(this, item)} className='right-btn lives'>
+                    <Image className='icons' mode='aspectFit' src={`${APP_IMAGE_CDN}/live_w.png`} />
+                    进入直播
+                  </View>
+                }
+                {
+                  notStarted &&
+                  <View onClick={this.onLocation.bind(this, item)} className='right-btn return notice-border'>
+                    <Image className='icons' mode='aspectFit' src={`${APP_IMAGE_CDN}/notice_b.png`} />
+                    查看预告
+                  </View>
+                }
+                {
+                  playback &&
+                  <View onClick={this.onLocation.bind(this, item)} className='right-btn return return-border'>
+                    <Image className='icons' mode='aspectFit' src={`${APP_IMAGE_CDN}/return_b.png`} />
+                    查看回放
+                  </View>
+                }
               </View>
-            }
-            {
-              ((item.live_status == 103 && item.close_replay == 1) || item.live_status == 104 || item.live_status == 105 || item.live_status == 106 || item.live_status == 107) &&
-              <View className='liveroom-page-state'>
-                <View className='live return'>已结束</View>
-                <View className='content'>直播已结束</View>
-              </View>
-            }
-          </View>
-        )) */}
-      {/* } */}
+            </View>
+          )
+        })
+      }
       {/* 加载更多 */}
       <LoadingMore isLoading={isLoading} isEnd={isEnd} isEmpty={isEmpty} />
       {/* 防止子内容无法支撑scroll-view下拉刷新 */}
