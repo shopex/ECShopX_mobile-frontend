41ed086206f2fcfb713a99695adf341313f0aa32
maqian <maqian@shopex.cn>
2021-10-21 22:45:31 +0800

    上传图片

--- src/marketing/pages/distribution/index.js
+++ src/marketing/pages/distribution/index.js
@@ -148,7 +148,7 @@ export default class DistributionDashboard extends Component {
     if (avatar) { // 头像
       avatarImg = await Taro.getImageInfo({src: avatar})
     }
-    const bck = await Taro.getImageInfo({src: `${APP_IMAGE_CDN}/distribution_bck.png`}) // 背景图片
+    const bck = await Taro.getImageInfo({src: 'https://b-img-cdn.yuanyuanke.cn/image/21/2021/10/21/9c8cbb5f6b6a346641fe151b5e1604118H6zDVOajmkASVYKgPePOYXIeFpc55Ta'}) // 背景图片
     const codeImg = await Taro.getImageInfo({src: wxappCode}) // 二维码
     if (avatarImg) {
       const posterImgs = {
