8a969016f734588f877389fe16ee289c0de674db
WJB3 <2459666474@qq.com>
2021-11-22 10:21:33 +0800

    fix:修复首页报错

--- src/pages/home/wgts/imghot-zone.js
+++ src/pages/home/wgts/imghot-zone.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View,Text } from '@tarojs/components'
 import { SpImg } from '@/components'
 import { linkPage } from './helper'
 
