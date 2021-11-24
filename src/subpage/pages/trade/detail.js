087ac3494e8825835ecc1e7436511f77a6920880
wangdongdongMac <wangdongdong9264@hotmail.com>
2021-11-24 11:36:21 +0800

    fix: css style

--- src/subpage/pages/trade/detail.js
+++ src/subpage/pages/trade/detail.js
@@ -301,10 +301,10 @@ export default class TradeDetail extends Component {
     }
 
     if(isWeb){
-      redirectUrl(api, `/subpage/pages/cashier/index?order_id=${order_id}`) 
+      redirectUrl(api, `/subpage/pages/cashier/index?order_id=${order_id}`)
       return ;
     }
-    
+
     const config = await api.cashier.getPayment(paymentParams)
 
     this.setState({
@@ -677,6 +677,7 @@ export default class TradeDetail extends Component {
                   分钟
                 </View>
               )}
+              <View>{info.status}</View>
               {info.status !== 'WAIT_BUYER_PAY' && (
                 <View className='delivery-infos'>
                   <View className='delivery-infos__status'>
