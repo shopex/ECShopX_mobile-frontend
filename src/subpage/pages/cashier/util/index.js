a2e1676ca0f1d2c016caa489d21653f0a1b9125c
WJB3 <2459666474@qq.com>
2021-12-08 15:13:58 +0800

    fix:修改

--- src/subpage/pages/cashier/util/index.js
+++ src/subpage/pages/cashier/util/index.js
@@ -4,27 +4,25 @@ export const SYMBOL = 'alipay_submit_div';
 
 //删除支付宝之前的form表单
 export function deleteForm() {
+
     let forms = document.getElementsByClassName(SYMBOL);
 
+    Taro.showToast({
+        icon: 'none',
+        title: '我执行了deleteForm操作',
+        duration: 15000
+    })
+
     while (forms.length) {
+
         console.log("===deleteForm===>");
 
         Taro.showToast({
             icon: 'none',
-            title: '我执行了deleteForm操作',
+            title: '我执行了删除表单操作',
             duration: 15000
         })
 
-        let forms = document.getElementsByClassName(SYMBOL);
-
-        while (forms.length) {
-            Taro.showToast({
-                icon: 'none',
-                title: '我执行了删除表单操作',
-                duration: 15000
-            })
-
-            document.body.removeChild(forms[0]);
-        }
+        document.body.removeChild(forms[0]);
     }
 }
