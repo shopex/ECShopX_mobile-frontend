125fe261d678532685edb1941dd042e8b1a88bc7
WJB3 <2459666474@qq.com>
2021-12-13 17:33:54 +0800

    fix:优惠券修改

--- src/others/pages/home/coupon-home.js
+++ src/others/pages/home/coupon-home.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, ScrollView, Image } from '@tarojs/components'
+import { View, ScrollView } from '@tarojs/components'
 import { Loading, SpNote, SpNavBar, SpToast, CouponItem } from '@/components'
 import { connect } from '@tarojs/redux'
 import api from '@/api'
