8cd68cb090360a5e59dedaa575f27ee3c2fb13b2
maqian <maqian@shopex.cn>
2021-12-29 10:55:42 +0800

    附近商家列表不刷新

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,9 +1,11 @@
 import useLogin from './useLogin'
 import usePage from './usePage'
+import useFirstMount from './useFirstMount'
 
 export default {}
 
 export {
   useLogin,
-  usePage
+  usePage,
+  useFirstMount
 }
