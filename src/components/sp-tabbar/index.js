a64d576d2275fa9d51f29c8755a9371c54c41815
maqian <maqian@shopex.cn>
2022-01-05 14:48:41 +0800

    调整

--- src/components/sp-tabbar/index.js
+++ src/components/sp-tabbar/index.js
@@ -23,7 +23,7 @@ function SpTabbar (props) {
       selectedImage: item.selectedIconPath,
       url: item.pagePath,
       urlRedirect: true,
-      text: item.text === '购物车' ? cartCount : '',
+      // text: item.text === '购物车' ? cartCount : null,
       max: item.max
     }
   })
