79db61c183adafb114b1e6c4e2d3179bc8696c76
WJB3 <2459666474@qq.com>
2022-01-11 14:07:20 +0800

    fix:增加品牌简介页面

--- src/pages/store/brand-info.js
+++ src/pages/store/brand-info.js
@@ -1,21 +1,73 @@
 import React, { useEffect } from 'react'
 import Taro, { getCurrentInstance, useDidShow } from '@tarojs/taro'
+import { ScrollView, View, Text } from '@tarojs/components'
 import { SpPage, SpLoading } from '@/components'
-import { classNames } from '@/utils'
+import { classNames , JumpPageIndex } from '@/utils'
+import CompHeader from './comps/comp-header'
+import { useImmer } from 'use-immer'
 import api from '@/api'
 import './brand-info.scss'
 
+const initialState = {
+  storeInfo: {}
+}
+
 const PageBrandInfo = () => {
+  const [state, setState] = useImmer(initialState)
+
   const $instance = getCurrentInstance()
+
   const {
     params: { distributor_id }
   } = $instance.router
 
+  const getDetail = async () => {
+    const {
+      name,
+      logo,
+      scoreList,
+      distributor_id: id,
+      introduce
+    } = await api.shop.getShop({
+      distributor_id,
+      show_score: 1,
+      show_marketing_activity: 1
+    })
+    setState((_state) => {
+      _state.storeInfo = {
+        name,
+        brand: logo,
+        scoreList,
+        distributor_id: id,
+        introduce
+      }
+    })
+  }
+
   useEffect(() => {
-    console.log('===distributor_id==', distributor_id)
+    getDetail()
   }, [])
 
-  return <SpPage className={classNames('page-store-brand')}></SpPage>
+  const { storeInfo } = state
+
+  return (
+    <SpPage className={classNames('page-store-brand')}>
+      <ScrollView className='page-store-brand-scrollview' scrollY>
+        <View className='margin'>
+          <CompHeader info={storeInfo} brand={false} />
+        </View>
+        <View className='margin padding brand'>
+          <View className='title'>品牌简介</View>
+          {/* <View className='content'>{storeInfo.introduce??'无'}</View> */}
+        </View>
+        <View className='margin good'>
+          <View className='title' onClick={JumpPageIndex}>
+            {'去看看全部商品 >'}
+          </View>
+        </View>
+      </ScrollView>
+    </SpPage>
+  )
 }
 
 export default PageBrandInfo
