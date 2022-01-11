72e6e4fac0d861bf59f7645c4d0935c52838e74a
maqian <maqian@shopex.cn>
2022-01-11 16:19:20 +0800

    comp-decomp-deliver组件调整

--- src/pages/cart/comps/comp-deliver.js
+++ src/pages/cart/comps/comp-deliver.js
@@ -1,60 +1,20 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import Taro, { getCurrentInstance } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import { useSelector } from 'react-redux'
-import { AddressChoose, SpPage } from '@/components'
+import { AddressChoose } from '@/components'
 
 import './comp-deliver.scss'
+import classNames from 'classnames'
 
 function CmopDeliver (props) {
   const {
     curStore = {},
     address = {},
-    isOpenStore = false,
-    receiptType = '',
-    headShop = {},
-    onChangReceiptType = () => {},
-    onEidtZiti = () => {}
+    receiptType = '', // 配送方式（logistics：普通快递  dada：同城配  ziti：自提）
+    headShop = {}, // 总店自提点
+    onChangReceiptType = () => {}
   } = props
-
-  const $instance = getCurrentInstance()
-  const router = $instance.router
-  const { goodType, type } = router.params
-
-  const { location = {} } = useSelector((state) => state.user)
-
-  const handleSwitchExpress = (type) => {
-    // 切换配送方式
-    if (receiptType === type) return false
-    onChangReceiptType(type)
-  }
-
-  const handleEditZitiClick = (id) => {
-    // 切换自提店铺
-    onEidtZiti(id)
-  }
-
-  const handleMapClick = () => {
-    // 点击地图icon
-    const { lat, lng } = location || {}
-    Taro.openLocation({
-      latitude: Number(lat),
-      longitude: Number(lng),
-      scale: 18
-    })
-  }
-
-  const handleChooseAddress = (choose) => {
-    // 自定义选择店铺跳转事件
-    let city = headShop.is_current ? headShop.city : curStore.city
-    let params = ''
-    if (receiptType === 'dada') {
-      params = `&city=${city}&receipt_type=${receiptType}`
-    }
-    Taro.navigateTo({
-      url: `/marketing/pages/member/address?isPicker=${choose}${params}`
-    })
-  }
 
   const deliveryList = [
     {
@@ -74,34 +34,67 @@ function CmopDeliver (props) {
     }
   ]
 
-  const showSwitchDeliver = deliveryList.filter((item) => item.isopen)
+  const showSwitchDeliver = deliveryList.filter((item) => item.isopen) || []
+
+  const $instance = getCurrentInstance()
+  const router = $instance.router
+  const { type } = router.params
+
+  const { location = {} } = useSelector((state) => state.user)
+
+  const handleSwitchExpress = (type) => {
+    // 切换配送方式
+    if (receiptType === type) return
+    onChangReceiptType(type)
+  }
+
+  const handleMapClick = () => {
+    // 点击地图icon
+    const { lat, lng } = location || {}
+    Taro.openLocation({
+      latitude: Number(lat),
+      longitude: Number(lng),
+      scale: 18
+    })
+  }
+
+  const handleChooseAddress = (choose) => {
+    // 自定义选择店铺跳转事件
+    let city = headShop.is_current ? headShop.city : curStore.city
+    let params = ''
+    if (receiptType === 'dada') {
+      params = `&city=${city}&receipt_type=dada`
+    }
+    Taro.navigateTo({
+      url: `/marketing/pages/member/address?isPicker=${choose}${params}`
+    })
+  }
 
   const showSwitchDeliverComp = () => {
-    if (showSwitchDeliver && showSwitchDeliver.length > 0) {
-      return (
-        <View className='switch-tab'>
-          {showSwitchDeliver.map((item) => (
-            <View
-              key={item.type}
-              className={`switch-item ${receiptType === item.type ? 'active' : ''}`}
-              onClick={handleSwitchExpress.bind(this, item.type)}
-            >
-              {item.name}
-            </View>
-          ))}
-        </View>
-      )
-    }
+    // 配送方式选择器
+    return (
+      <View className={classNames(showSwitchDeliver.length > 0 && 'switch-tab')}>
+        {showSwitchDeliver.map((item) => (
+          <View
+            key={item.type}
+            className={`switch-item ${receiptType === item.type ? 'active' : ''}`}
+            onClick={handleSwitchExpress.bind(this, item.type)}
+          >
+            {item.name}
+          </View>
+        ))}
+      </View>
+    )
   }
 
   return (
-    <SpPage className='comp-deliver'>
+    <View className='page-comp-deliver'>
       {showSwitchDeliverComp()}
       {/** 普通快递 */}
       {receiptType === 'logistics' && <AddressChoose isAddress={address} />}
-      {/** 同城配送 */}
+      {/** 同城配 */}
       {receiptType === 'dada' && (
-        <View className='cityDeliver'>
+        <View className='store-module'>
           <AddressChoose isAddress={address} onCustomChosse={handleChooseAddress.bind(this)} />
           <View className='store'>
             配送门店: {headShop.is_current ? headShop.name : curStore.name}
@@ -111,19 +104,12 @@ function CmopDeliver (props) {
       {/** 自提 */}
       {receiptType === 'ziti' && (
         <View className='address-module'>
-          <View className='addr-title'>{curStore.name}</View>
-          <View className='addr-detail'>
+          <View className='address-title'>{curStore.name}</View>
+          <View className='address-detail'>
             <View className='address'>{curStore.store_address}</View>
-            {isOpenStore && process.env.APP_PLATFORM === 'standard' ? (
-              <View
-                className='iconfont icon-edit'
-                onClick={handleEditZitiClick.bind(this, curStore.distributor_id)}
-              ></View>
-            ) : (
-              <View className='iconfont icon-periscope' onClick={handleMapClick.bind(this)}></View>
-            )}
+            <View className='iconfont icon-periscope' onClick={handleMapClick.bind(this)}></View>
           </View>
-          <View className='otherInfo'>
+          <View className='other-info'>
             <View className='text-muted light'>门店营业时间：{curStore.hour}</View>
             <View className='text-muted'>
               联系电话：
@@ -132,7 +118,7 @@ function CmopDeliver (props) {
           </View>
         </View>
       )}
-    </SpPage>
+    </View>
   )
 }
 
