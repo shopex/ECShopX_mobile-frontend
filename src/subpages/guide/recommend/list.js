558f1d7f891fc8148c2aea0b2dbb10f11e432153
lukaijie <lukaijie@shopex.cn>
2022-03-17 18:54:59 +0800

    merge

--- src/subpages/guide/recommend/list.js
+++ src/subpages/guide/recommend/list.js
@@ -1,191 +1,60 @@
-import React, { Component } from 'react'
-import Taro, { getCurrentInstance } from '@tarojs/taro'
-import { View, ScrollView } from '@tarojs/components'
-import { withPager, withBackToTop } from '@/hocs'
-import { SearchBar, RecommendItem, SpNote, Loading } from '@/components'
-import { BaTabBar, BaNavBar } from '../components'
-import api from '@/api'
-import S from '@/subpages/guide/lib/Spx.js'
-import { pickBy, styleNames } from '@/utils'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import Taro from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import { SpPage } from '@/components'
+import {
+  BaHomeWgts,
+  BaStoreList,
+  BaStore,
+  BaGoodsBuyPanel,
+  BaTabBar,
+  BaNavBar,
+  BaCoupon
+} from '@/subpages/guide/components'
 import './list.scss'
 
-@withPager
-@withBackToTop
-export default class RecommendList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      ...this.state,
-      query: null,
-      list: [],
-      isShowSearch: false
-    }
-  }
+function GuideRecommendList(props) {
+  useEffect(() => {}, [])
 
-  async componentDidMount() {
-    // await S.autoLogin(this)
-    this.nextPage()
-  }
-
-  config = {
-    navigationBarTitleText: '种草',
-    navigationStyle: 'custom'
-  }
-
-  componentDidShow() {}
-
-  // 搜索查询
-  handleSearchOn = () => {
-    this.setState({
-      isShowSearch: true
-    })
-  }
-  handleSearchOff = () => {
-    this.setState({
-      isShowSearch: false
-    })
-  }
-  handleSearchChange = (val) => {
-    this.setState({
-      query: {
-        ...this.state.query,
-        title: val
-      }
-    })
-  }
-  handleSearchClear = () => {
-    this.setState(
-      {
-        isShowSearch: false,
-        query: {
-          ...this.state.query,
-          title: ''
-        }
-      },
-      () => {
-        this.resetPage()
-        this.setState(
-          {
-            list: []
-          },
-          () => {
-            this.nextPage()
-          }
-        )
-      }
-    )
-  }
-  handleConfirm = (val) => {
-    this.setState(
-      {
-        isShowSearch: false,
-        query: {
-          ...this.state.query,
-          title: val
-        }
-      },
-      () => {
-        this.resetPage()
-        this.setState(
-          {
-            list: []
-          },
-          () => {
-            this.nextPage()
-          }
-        )
-      }
-    )
-  }
-
-  async fetch(params) {
-    const { page_no: page, page_size: pageSize } = params
-    const article_query = {
-      article_type: 'bring',
-      ...this.state.query,
-      page,
-      pageSize
-    }
-
-    const { list, total_count: total } = await api.article.list(article_query)
-    if (list.length) {
-      const nList = pickBy(list, {
-        img: 'image_url',
-        item_id: 'article_id',
-        title: 'title',
-        author: 'author',
-        summary: 'summary',
-        head_portrait: 'head_portrait',
-        isPraise: 'isPraise',
-        articlePraiseNum: 'articlePraiseNum.count'
-      })
-
-      this.setState({
-        list: [...this.state.list, ...nList]
-      })
-    }
-    return {
-      total
-    }
-  }
-
-  handleClickItem = (item) => {
-    const url = `/subpages/guide/recommend/detail?id=${item.item_id}`
-    Taro.navigateTo({
-      url
-    })
-  }
-
-  render() {
-    const { query, isShowSearch, scrollTop, list, page } = this.state
-    const n_ht = S.get('navbar_height', true)
-    const c_ht = n_ht + 42
-
-    return (
-      <View className='guide-recommend-list'>
-        <BaNavBar title='种草' fixed />
-        <View style={styleNames({ height: `${n_ht}px` })}></View>
-        <View
-          className={`recommend-list__search ${
-            query && query.title && isShowSearch ? 'on-search' : null
-          }`}
-        >
-          <SearchBar
-            showDailog={false}
-            keyword={query ? query.title : ''}
-            onFocus={this.handleSearchOn}
-            onChange={this.handleSearchChange}
-            onClear={this.handleSearchClear}
-            onCancel={this.handleSearchOff}
-            onConfirm={this.handleConfirm.bind(this)}
-          />
-        </View>
-
-        <ScrollView
-          className='recommend-list__scroll'
-          scrollY
-          scrollTop={scrollTop}
-          scrollWithAnimation
-          onScroll={this.handleScroll}
-          onScrollToLower={this.nextPage}
-          style={styleNames({ top: `${c_ht}px` })}
-        >
-          <View className='recommend-list recommend-list__type-grid'>
-            {list.map((item) => {
-              return (
-                <View className='recommend-list__item' key={item.item_id}>
-                  <RecommendItem info={item} onClick={() => this.handleClickItem(item)} noShowZan />
-                </View>
-              )
-            })}
-          </View>
-          {page.isLoading ? <Loading>正在加载...</Loading> : null}
-          {!page.isLoading && !page.hasNext && !list.length && (
-            <SpNote img='trades_empty.png'>暂无数据~</SpNote>
-          )}
-        </ScrollView>
-        <BaTabBar />
+  return (
+    <SpPage className='page-guide-recommendlist'>
+      <BaNavBar home title='种草' />
+      <View className='search-wrap'>
+        <SpSearchBar
+          keyword={keywords}
+          placeholder='搜索'
+          onFocus={handleOnFocus}
+          onChange={handleOnChange}
+          onClear={handleOnClear}
+          onCancel={handleSearchOff}
+          onConfirm={handleConfirm}
+        />
       </View>
-    )
-  }
+      <SpScrollView className='item-list-scroll' ref={goodsRef} fetch={fetch}>
+        {/* <View className='goods-list'>
+          <View className='left-container'>
+            {leftList.map((item, index) => (
+              <View className='goods-item-wrap' key={`goods-item__${index}`}>
+                <SpGoodsItem showFav onStoreClick={handleClickStore} info={item} />
+              </View>
+            ))}
+          </View>
+          <View className='right-container'>
+            {rightList.map((item, index) => (
+              <View className='goods-item-wrap' key={`goods-item__${index}`}>
+                <SpGoodsItem showFav onStoreClick={handleClickStore} info={item} />
+              </View>
+            ))}
+          </View>
+        </View> */}
+      </SpScrollView>
+    </SpPage>
+  )
 }
+
+GuideRecommendList.options = {
+  addGlobalClass: true
+}
+
+export default GuideRecommendList
