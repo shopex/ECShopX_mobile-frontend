2068efb0274aceb3d15054c1e1738a5b082fb0b2
lukaijie <lukaijie@shopex.cn>
2022-04-06 22:54:27 +0800

    publicpath

--- config/index.js
+++ config/index.js
@@ -98,7 +98,8 @@ const config = {
   },
 
   h5: {
-    publicPath: IS_PROD ? './' : '/',
+    // publicPath: IS_PROD ? './' : '/',
+    publicPath: '/',
     // publicPath: (IS_APP && IS_PROD) ? './' : '/',
     // publicPath: process.env.APP_PUBLIC_PATH || '/',
     staticDirectory: 'static',
