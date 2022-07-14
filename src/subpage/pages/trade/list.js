dc9672bd098bfc6953f0188db599ba95d8ee5b3a
shopex-ci <shopex-ci@shopex.cn>
2022-07-14 18:29:24 +0800

    update

--- src/subpage/pages/trade/list.js
+++ src/subpage/pages/trade/list.js
@@ -37,8 +37,7 @@ export default class TradeList extends Component {
       tabList: [
         { title: '全部订单', status: '0' },
         { title: '待支付', status: '5' },
-        { title: '待收货', status: '1' },
-        
+        { title: '待收货', status: '1' }
       ],
       // 是否显示待评价
       evaluate: 1,
@@ -50,11 +49,11 @@ export default class TradeList extends Component {
 
   componentDidShow() {
     const { status, evaluate = 1 } = this.$instance.router.params
-    const tabIdx = this.state.tabList.findIndex((tab) => tab.status === status)
     const _tabList = JSON.parse(JSON.stringify(this.state.tabList))
-    if(evaluate == 1) {
+    if (evaluate == 1) {
       _tabList.push({ title: '待评价', status: '7', is_rate: 0 })
     }
+    const tabIdx = _tabList.findIndex((tab) => tab.status === status)
     if (tabIdx >= 0) {
       this.setState(
         {
@@ -311,7 +310,6 @@ export default class TradeList extends Component {
       rateStatus,
       evaluate
     } = this.state
-
 
     return (
       <View
