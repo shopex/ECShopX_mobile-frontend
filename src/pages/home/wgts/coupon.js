829e990092ad327e6b260c34eef0958ea0598a62
lukaijie <lukaijie@shopex.cn>
2022-07-18 18:40:07 +0800

    fix: 内购

---
+++ src/pages/home/wgts/coupon.js
@@ -0,0 +1,182 @@
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useImmer } from 'use-immer'
+import Taro from '@tarojs/taro'
+import api from '@/api'
+import doc from '@/doc'
+import { View, Text } from '@tarojs/components'
+import { SpImage } from '@/components'
+import { classNames, styleNames, isArray } from '@/utils'
+import './coupon.scss'
+
+function WgtCoupon(props) {
+  const { base, data, voucher_package } = props.info
+  useEffect(() => {}, [])
+
+  const len = data.length + voucher_package.length
+
+  const renderCouponCell1 = () => {
+    return (
+      <View className='coupon-wrap coupon-1'>
+        {data.map((item, index) => (
+          <View
+            className='coupon-item'
+            style={styleNames({
+              'background-image': `url(${process.env.APP_IMAGE_CDN}/coupon_one.png)`
+            })}
+            key={`coupon-item__${index}`}
+          >
+            <View className='coupon-hd'>
+              {item.type == 'cash' && (
+                <View className='amount'>
+                  <Text className='symbol'>¥</Text>
+                  <Text className='value'>{item.amount}</Text>
+                </View>
+              )}
+              {item.type == 'discount' && (
+                <View className='amount'>
+                  <Text className='value'>{item.amount}</Text>
+                  <Text className='symbol'>折</Text>
+                </View>
+              )}
+              <View className='desc'>{item.desc}</View>
+            </View>
+            <View className='coupon-ft'>
+              <View className='title'>{item.title}</View>
+              <View className='name'>
+                {
+                  {
+                    'discount': '折扣券',
+                    'cash': '优惠券'
+                  }[item.type]
+                }
+              </View>
+            </View>
+          </View>
+        ))}
+        {voucher_package.map((item, index) => (
+          <View
+            className='coupon-item coupon-package'
+            style={styleNames({
+              'background-image': `url(${process.env.APP_IMAGE_CDN}/coupon_one.png)`
+            })}
+            key={`coupon-package-item__${index}`}
+          >
+            <View className='coupon-hd'>
+              <View className='amount'>券</View>
+              <View className='desc'>{item.package_describe}</View>
+            </View>
+            <View className='coupon-ft'>
+              <View className='title'>{item.title}</View>
+              <View className='name'>券包</View>
+            </View>
+          </View>
+        ))}
+      </View>
+    )
+  }
+
+  const renderCouponCell2 = () => {
+    return (
+      <View className={classNames('coupon-wrap', `coupon-${len > 4 ? 4 : len}`)}>
+        {data.map((item, index) => {
+          if (item.imgUrl) {
+            return <SpImage className='coupon-image' src={item.imgUrl} width={710} />
+          } else {
+            let backgroundImg = 'coupon_two.png'
+            if (len == 3) {
+              backgroundImg = 'coupon_three.png'
+            } else if (len >= 4) {
+              backgroundImg = 'coupon_four.png'
+            }
+            return (
+              <View
+                className='coupon-item'
+                style={styleNames({
+                  'background-image': `url(${process.env.APP_IMAGE_CDN}/${backgroundImg})`
+                })}
+                key={`coupon-item__${index}`}
+              >
+                <View className='coupon-hd'>
+                  {item.type == 'cash' && (
+                    <View className='amount'>
+                      <Text className='symbol'>¥</Text>
+                      <Text className='value'>{item.amount}</Text>
+                    </View>
+                  )}
+                  {item.type == 'discount' && (
+                    <View className='amount'>
+                      <Text className='value'>{item.amount}</Text>
+                      <Text className='symbol'>折</Text>
+                    </View>
+                  )}
+                </View>
+                <View className='coupon-ft'>
+                  <View className='title'>{item.title}</View>
+                  <View className='name'>
+                    {
+                      {
+                        'discount': '折扣券',
+                        'cash': '优惠券'
+                      }[item.type]
+                    }
+                  </View>
+                </View>
+              </View>
+            )
+          }
+        })}
+        {voucher_package.map((item, index) => {
+          if (item.imgUrl) {
+            return <SpImage className='coupon-image' src={item.imgUrl} width={710} />
+          } else {
+            return (
+              <View
+                className='coupon-item coupon-package'
+                style={styleNames({
+                  'background-image': `url(${process.env.APP_IMAGE_CDN}/coupon_four.png)`
+                })}
+                key={`coupon-package-item__${index}`}
+              >
+                <View className='coupon-hd'>
+                  <View className='amount'>券</View>
+                </View>
+                <View className='coupon-ft'>
+                  <View className='title'>{item.title}</View>
+                  <View className='name'>券包</View>
+                </View>
+              </View>
+            )
+          }
+        })}
+      </View>
+    )
+  }
+
+  return (
+    <View
+      className={classNames('wgt wgt-coupon', {
+        wgt__padded: base.padded
+      })}
+    >
+      {base.title && (
+        <View className='wgt-head'>
+          <View className='wgt-hd'>
+            <Text className='wgt-title'>{base.title}</Text>
+            <Text className='wgt-subtitle'>{base.subtitle}</Text>
+          </View>
+        </View>
+      )}
+      <View className='wgt__body with-padding'>
+        {len == 1 && renderCouponCell1()}
+        {len >= 2 && renderCouponCell2()}
+      </View>
+    </View>
+  )
+}
+
+WgtCoupon.options = {
+  addGlobalClass: true
+}
+
+export default WgtCoupon
