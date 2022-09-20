507de4bd97d5fcf90634e9c365de3e345b6d169e
lukaijie <lukaijie@shopex.cn>
2022-09-20 21:27:47 +0800

    sp-login 改造

--- src/components/sp-login/index.weapp.js
+++ src/components/sp-login/index.weapp.js
@@ -1,13 +1,13 @@
 import Taro from '@tarojs/taro'
 import React, { useEffect, useState, useCallback } from 'react'
 import { View, Button } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { AtButton, AtCurtain } from 'taro-ui'
 import S from '@/spx'
 import api from '@/api'
 import { isWeixin, isAlipay, classNames, showToast, entryLaunch } from '@/utils'
 import { SG_SHARER_UID, SG_TRACK_PARAMS, SG_ROUTER_PARAMS, SG_GUIDE_PARAMS } from '@/consts'
 import { Tracker } from '@/service'
-import { SpPrivacyModal } from '@/components'
+import { SpPrivacyModal, SpImage } from '@/components'
 import { useLogin } from '@/hooks'
 import './index.scss'
 
@@ -21,6 +21,7 @@ function SpLogin(props) {
 
   const [isNewUser, setIsNewUser] = useState(false)
   const [policyModal, setPolicyModal] = useState(false)
+  const [loginModal, setLoginModal] = useState(false)
 
   useEffect(() => {
     if (newUser) {
@@ -67,7 +68,7 @@ function SpLogin(props) {
         params['uid'] = uid
       }
       // gu_user_id: 欢迎语上带过来的员工编号, 同work_user_id
-      if(gu_user_id) {
+      if (gu_user_id) {
         params['channel'] = 1
         params['work_userid'] = gu_user_id
       }
@@ -105,10 +106,23 @@ function SpLogin(props) {
     }
     setPolicyModal(false)
   }, [])
+
+  const handleClick = () => {
+    if (isLogin) {
+      onChange && onChange()
+    } else {
+      setLoginModal(true)
+    }
+  }
+
   console.log('newUser:', newUser, isNewUser)
+
+  const { icon, nickname } = __wxConfig.accountInfo
+
   return (
     <View className={classNames('sp-login', className)}>
-      {isLogin && <View onClick={handleOnChange}>{children}</View>}
+      <View onClick={handleClick}>{children}</View>
+      {/* {isLogin && <View onClick={handleOnChange}>{children}</View>}
 
       {!isLogin && isNewUser && (
         <Button className='login-btn' openType='getPhoneNumber' onGetPhoneNumber={handleBindPhone}>
@@ -116,13 +130,26 @@ function SpLogin(props) {
         </Button>
       )}
 
-      {!isLogin && !isNewUser && <View onClick={handleClickLogin}>{children}</View>}
+      {!isLogin && !isNewUser && <View onClick={handleClickLogin}>{children}</View>} */}
 
       <SpPrivacyModal
         open={policyModal}
         onCancel={handleCloseModal}
         onConfirm={handleConfirmModal}
       />
+
+      <AtCurtain isOpened={loginModal}>
+        <View className='login-modal'>
+          <View className='login-modal__hd'>
+            <SpImage circle src={icon} width={120} height={120} />
+            <View className='nick-name'>{nickname}</View>
+          </View>
+          <View className='login-modal__bd'>登录手机号，查看全部订单和优惠券</View>
+          <View className='login-modal__ft'>
+            <AtButton type='primary' />
+          </View>
+        </View>
+      </AtCurtain>
     </View>
   )
 }
