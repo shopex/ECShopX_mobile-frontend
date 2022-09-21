dfeb39814032c295e97b4fbbfeac2fa87cfe4c74
maqian <maqian@shopex.cn>
2022-09-21 17:48:23 +0800

    退货物流添-新增极兔速递&丰网速运

--- src/consts/index.js
+++ src/consts/index.js
@@ -212,6 +212,24 @@ export const GOODS_TYPE = {
   'package': '组合商品'
 }
 
+export const LOGISTICS_CODE = {
+  'SF': '顺丰速运',
+  'HTKY': '百世快递',
+  'ZTO': '中通快递',
+  'STO': '申通快递',
+  'YTO': '圆通速递',
+  'YD': '韵达快递',
+  'YZPY': '邮政快递包裹',
+  'EMS': 'EMS',
+  'HHTT': '天天快递',
+  'JD': '京东快递',
+  'UC': '优速快递',
+  'DBL': '德邦快递',
+  'ZJS': '宅急送',
+  'FWX': '丰网速运',
+  'JTSD': '极兔速递'
+}
+
 export * from './localstorage'
 
 export default {}
