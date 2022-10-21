3b6598b048d354db52a475d3670e6b36badd6f9c
zhoumin <zhoumin@shopex.cn>
2022-10-21 13:39:18 +0800

    fix: 支付宝小程序兼容

--- src/api/shop.js
+++ src/api/shop.js
@@ -43,7 +43,7 @@ export function getPageParamsConfig ({
   template_name = platformTemplateName,
   version = 'v1.0.1'
 }) {
-  return req.get(transformPlatformUrl('/pageparams/setting'), {
+  return req.get('/pageparams/setting', {
     page_name,
     template_name,
     version
