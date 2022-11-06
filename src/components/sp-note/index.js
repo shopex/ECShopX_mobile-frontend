856974af97d7af3d07c7e2d717f44c55c15659cf
wangzhanyuan <dreamworks.cnn@gmail.com>
2022-11-06 19:38:02 +0800

    fix: ECX-3620,缺省页须统一更新为新版样式，涉及页面：种草; ECX-3579,详情到货通知，点击后报错;

--- src/components/sp-note/index.js
+++ src/components/sp-note/index.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: error: git config user.email & please set dead value or install git
+ * @Date: 2022-02-28 00:39:47
+ * @LastEditors: error: git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-11-06 19:34:25
+ * @FilePath: /ecshopxx-vshop/src/components/sp-note/index.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by error: git config user.name && git config user.email & please set dead value or install git, All Rights Reserved. 
+ */
 import React, { Component } from 'react'
 import { View, Text, Image } from '@tarojs/components'
 import { AtIcon, AtButton } from 'taro-ui'
@@ -37,12 +47,12 @@ export default class SpNote extends Component {
       <View className={classNames('sp-note', className)}>
         {/* {icon && <AtIcon prefixClass='sw-icon' value={value} size='60' color='#cdcdcd' />} */}
         {img && (
-          <SpImage className='default-img' src={img} width={292} height={224} />
-          // <Image
-          //   className='sp-note__img'
-          //   mode='widthFix'
-          //   src={isUrl ? img : this.resolveUrl(img)}
-          // />
+          isUrl ? <SpImage className='default-img' src={img} width={292} height={224} /> :
+          <Image
+            className='sp-note__img'
+            mode='widthFix'
+            src={isUrl ? img : this.resolveUrl(img)}
+          />
         )}
         <Text className='sp-note__text'>{title || this.props.children}</Text>
         {button && to && (
