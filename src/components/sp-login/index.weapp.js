fe21f759cfab818581a36c34878f3dfe50ebca07
lukaijie <lukaijie@shopex.cn>
2023-01-13 12:21:45 +0800

    fix: 朋友圈单页模式

--- src/components/sp-login/index.weapp.js
+++ src/components/sp-login/index.weapp.js
@@ -113,6 +113,10 @@ function SpLogin(props, ref) {
 
   // 登录
   const handleClickLogin = async () => {
+    const { scene } = Taro.getLaunchOptionsSync()
+    if(scene == 1154) {
+      return showToast('请前往小程序使用完整服务')
+    }
     const checkRes = await checkPolicyChange()
     if (!checkRes) {
       setPolicyModal(true)
