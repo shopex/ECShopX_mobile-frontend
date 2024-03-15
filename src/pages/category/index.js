c3a2d88c9452cb6e4c647e91a5e653cb72746930
chenxin <398275325@qq.com>
2024-03-15 18:41:01 +0800

    老版本分类

--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -1,16 +1,12 @@
-import React, { useRef, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import Taro, { useDidShow, getCurrentInstance } from '@tarojs/taro'
-import { Text, View, Image, ScrollView } from '@tarojs/components'
+import React, { useEffect } from 'react'
+import Taro from '@tarojs/taro'
+import {View} from '@tarojs/components'
 import { useImmer } from 'use-immer'
-import { SpScrollView, SpPage, SpTabbar, SpCategorySearch, SpSkuSelect, SpLogin } from '@/components'
 import api from '@/api'
-import doc from '@/doc'
-import { useDebounce } from '@/hooks'
-import S from '@/spx'
-import { pickBy, classNames, styleNames, showToast, entryLaunch } from '@/utils'
-import './index.scss'
 import CompsCategoryOld from './comps/comps-category-old'
+import CompsCategoryNew from './comps/comps-category-new'
+
+import './index.scss'
 
 const initialState = {
   keywords: ''
@@ -18,7 +14,6 @@ const initialState = {
 
 function StoreItemList(props) {
   const [state, setState] = useImmer(initialState)
-  // const { purchase_share_info = {} } = useSelector((state) => state.purchase)
   const {
     keywords,
   } = state
@@ -26,8 +21,6 @@ function StoreItemList(props) {
   useEffect(() => {
     getCategoryList()
   }, [])
-
-  
 
   const getCategoryList = async () => {
     const res = await api.category.getCommonSetting()
@@ -37,11 +30,10 @@ function StoreItemList(props) {
     })
   }
 
-
   return (
-    <View className=''>
+    <View>
       {
-        keywords?"":<CompsCategoryOld></CompsCategoryOld>
+        keywords?<CompsCategoryNew />:<CompsCategoryOld />
       }
     </View>
   )
