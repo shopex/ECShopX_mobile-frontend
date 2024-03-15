15b135a32a85ea9fe26010f6735503ce502f29c1
chenxin <398275325@qq.com>
2024-03-15 18:11:58 +0800

    购物车切换完成新版本

--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -9,368 +9,41 @@ import doc from '@/doc'
 import { useDebounce } from '@/hooks'
 import S from '@/spx'
 import { pickBy, classNames, styleNames, showToast, entryLaunch } from '@/utils'
-import CompFirstCategory from './comps/comp-first-category'
-import CompSecondCategory from './comps/comp-second-category'
-import CompThirdCategory from './comps/comp-third-category'
-import CompGoodsItem from './comps/comp-goods-item'
 import './index.scss'
-
-const MSpSkuSelect = React.memo(SpSkuSelect)
+import CompsCategoryOld from './comps/comps-category-old'
 
 const initialState = {
-  cusIndex: 1,
-  keywords: '',
-  allList: [],
-  categoryFirstIndex: 0,
-  categorySecondIndex: 0,
-  categoryThirdIndex: 0,
-  goodsSort: 0,
-  seriesList: [],
-  cat_id: undefined,
-  show: false,
-  secondList: [],
-  thirdList: [],
-  info: null,
-  skuPanelOpen: false,
-  selectType: 'picker'
+  keywords: ''
 }
 
 function StoreItemList(props) {
-  const $instance = getCurrentInstance()
   const [state, setState] = useImmer(initialState)
   // const { purchase_share_info = {} } = useSelector((state) => state.purchase)
   const {
     keywords,
-    cusIndex,
-    allList,
-    goodsSort,
-    seriesList,
-    categoryFirstIndex,
-    categorySecondIndex,
-    categoryThirdIndex,
-    cat_id,
-    secondList,
-    thirdList,
-    info,
-    skuPanelOpen,
-    selectType
   } = state
 
-  const goodsRef = useRef()
-  const pageRef = useRef()
-  const loginRef = useRef()
-  const dispatch = useDispatch()
   useEffect(() => {
     getCategoryList()
   }, [])
 
-  useEffect(() => {
-    if (cat_id) {
-      goodsRef?.current.reset()
-      changeList()
-    }
-  }, [cat_id])
-
-  useEffect(() => {
-    if (skuPanelOpen) {
-      pageRef.current.pageLock()
-    } else {
-      pageRef.current.pageUnLock()
-    }
-  }, [skuPanelOpen])
+  
 
   const getCategoryList = async () => {
-    //api.category.getCategory这个接口会导致必要的category_id不存在，根据汪海的建议换了下面的接口获取数据
-    // let currentList = []
-    // const query = { template_name: platformTemplateName, version: 'v1.0.1', page_name: 'category' }
-    // const { list } = await api.category.getCategory(query)
-    // currentList = pickBy(list[0] ? list[0].params.data[0].content : [], {
-    //   name: 'name',
-    //   img: 'img',
-    //   id: 'id',
-    //   category_id: 'category_id',
-    //   children: ({ children }) =>
-    //     pickBy(children, {
-    //       name: 'name',
-    //       img: 'img',
-    //       id: 'id',
-    //       category_id: 'category_id',
-    //       children: ({ children: children_ }) =>
-    //         pickBy(children_, {
-    //           name: 'name',
-    //           img: 'img',
-    //           id: 'id',
-    //           category_id: 'category_id',
-    //         })
-    //     })
-    // })
-
-    // if (currentList.length!==0) {
-    const res = await api.category.get()
-    const currentList = pickBy(res, {
-      name: 'category_name',
-      img: 'image_url',
-      id: 'category_id',
-      category_id: 'category_id',
-      children: ({ children }) =>
-        pickBy(children, {
-          name: 'category_name',
-          img: 'image_url',
-          id: 'category_id',
-          category_id: 'category_id',
-          children: ({ children: children_ }) =>
-            pickBy(children_, {
-              name: 'category_name',
-              img: 'image_url',
-              id: 'category_id'
-            })
-        })
-    })
-    // }
+    const res = await api.category.getCommonSetting()
+    //category  老的
     setState((draft) => {
-      draft.seriesList = currentList
-      draft.hasSeries = true
-      draft.cat_id = currentList[0].id
+      draft.keywords = res.category_style == 'category'?true:false
     })
   }
 
-  const fetch = async ({ pageIndex, pageSize }) => {
-    const { dis_id = null } = $instance.router.params
-    const params = {
-      page: pageIndex,
-      pageSize,
-      keywords: keywords,
-      approve_status: 'onsale,only_show',
-      item_type: 'normal',
-      is_point: 'false',
-      distributor_id: dis_id || Taro.getStorageSync('distributor_id'),
-      goodsSort,
-      category_id: cat_id,
-      v_store: cusIndex
-    }
-
-    const { list: _list, total_count } = await api.item.search(params)
-
-    let n_list = pickBy(_list, doc.goods.ITEM_LIST_GOODS)
-
-    setState((draft) => {
-      draft.allList = pageIndex == 1 ? n_list : [...allList, ...n_list]
-    })
-
-    return { total: total_count }
-  }
-
-  const handleClickStore = (item) => {
-    const url = `/pages/indexStore?id=${item.distributor_info.distributor_id}`
-    Taro.navigateTo({
-      url
-    })
-  }
-
-  const handleConfirm = async (val) => {
-    console.log('handleConfirm', val)
-    if (val) {
-      Taro.navigateTo({
-        url: `/pages/item/list?keywords=${val}`
-      })
-    }
-  }
-
-  const onFirstCategoryClick = useDebounce((index) => {
-    if (categoryFirstIndex == index) return
-    setState((draft) => {
-      draft.categoryFirstIndex = index
-      draft.categorySecondIndex = 0
-      draft.categoryThirdIndex = 0
-      draft.allList = []
-      draft.cat_id = seriesList[index]?.id
-    })
-  }, 200)
-
-  const onSecondCategoryClick = useDebounce((index) => {
-    if (categorySecondIndex == index) return
-    setState((draft) => {
-      draft.categorySecondIndex = index
-      draft.categoryThirdIndex = 0
-      draft.allList = []
-      draft.cat_id = index == 0 ? seriesList[categoryFirstIndex]?.id : secondList[index]?.id
-    })
-  }, 200)
-
-  const onThirdCategoryClick = useDebounce((index) => {
-    if (categoryThirdIndex == index) return
-    setState((draft) => {
-      draft.categoryThirdIndex = index
-      draft.allList = []
-      draft.cat_id = index == 0 ? secondList[categorySecondIndex]?.id : thirdList[index]?.id
-    })
-  }, 200)
-
-  const onSelectClick = () => {
-    setState((draft) => {
-      draft.cusIndex = cusIndex === 1 ? 0 : 1
-      draft.allList = []
-    })
-    goodsRef?.current.reset()
-  }
-
-  const shoppingCart = async (item, e) => {
-    e.stopPropagation()
-    let params = {
-      item_id: item.itemId,
-      num: 1,
-      shop_type: 'distributor'
-    }
-    console.log('item44444ttttttt', item)
-    if (item.cart_num >= Number(item.activity_store)) {
-      return showToast(`最多加购${item.activity_store}件`)
-    }
-    await api.purchase.addPurchaseCart(params)
-    showToast('加入购物车成功')
-    // let changeList = JSON.parse(JSON.stringify(newList))
-    // changeList.map(l=>{
-    //   if(l.item_id == item.item_id){
-    //     l.cart_num = Number(l.cart_num) + 1
-    //   }
-    // })
-    // await setState((draft) => {
-    //   draft.newList = changeList
-    // })
-  }
-
-
-  const handleAddToCart = async ({ itemId, distributorId }) => {
-    if(!S.getAuthToken()) {
-      loginRef.current?.handleToLogin()
-      return
-    }
-
-    Taro.showLoading()
-    try {
-      const itemDetail = await api.item.detail(itemId, {
-        showError: false,
-        distributor_id: distributorId
-      })
-      Taro.hideLoading()
-      setState((draft) => {
-        draft.info = pickBy(itemDetail, doc.goods.GOODS_INFO)
-        draft.skuPanelOpen = true
-        draft.selectType = 'addcart'
-      })
-    } catch (e) {
-      showToast(e.message)
-      Taro.hideLoading()
-    }
-  }
-
-  const changeList = async () => {
-    const _secondList = [
-      {
-        name: '全部',
-        img: '',
-        id: ''
-      },
-      ...(seriesList[categoryFirstIndex]?.children || [])
-    ]
-    const _thirdList = _secondList[categorySecondIndex]?.children || []
-
-    setState((draft) => {
-      draft.secondList = _secondList
-      draft.thirdList =
-        _thirdList.length > 0
-          ? [
-            {
-              name: '全部',
-              img: '',
-              id: ''
-            },
-            ..._thirdList
-          ]
-          : []
-    })
-  }
 
   return (
-    <SpPage
-      scrollToTopBtn
-      className={classNames('page-category-index')}
-      renderFooter={<SpTabbar />}
-      ref={pageRef}
-    >
-      <View className='container-hd'>
-        <View className='category-search'>
-          <SpCategorySearch onConfirm={handleConfirm} />
-        </View>
-        <CompFirstCategory
-          cusIndex={categoryFirstIndex}
-          list={seriesList}
-          onClick={onFirstCategoryClick}
-        />
-      </View>
-      <View className='container-bd'>
-
-        <View className='left-container'>
-          <CompSecondCategory
-            cusIndex={categorySecondIndex}
-            list={secondList}
-            onClick={onSecondCategoryClick}
-          />
-        </View>
-
-        <View
-          className='right-container'
-          style={styleNames({
-            paddingTop: thirdList.length == 0 && '0px'
-          })}
-        >
-          {thirdList.length > 0 && (
-            <View className='right-container-fixed'>
-              <CompThirdCategory
-                cusIndex={categoryThirdIndex}
-                list={thirdList}
-                onClick={onThirdCategoryClick}
-                typeIndex={cusIndex}
-              />
-            </View>
-          )}
-          <ScrollView className='goods-list-container' scrollY>
-            <SpScrollView
-              className='scroll-view-goods'
-              ref={goodsRef}
-              fetch={fetch}
-              auto={false}
-            >
-              {allList.map((item, index) => (
-                <View className='goods-item-wrap' key={`goods-item-l__${index}`}>
-                  <CompGoodsItem onStoreClick={handleClickStore} onAddToCart={handleAddToCart} hideStore info={item} />
-                </View>
-              ))}
-            </SpScrollView>
-          </ScrollView>
-
-        </View>
-      </View>
-
-      {/* Sku选择器 */}
-      <MSpSkuSelect
-        open={skuPanelOpen}
-        type={selectType}
-        info={info}
-        onClose={() => {
-          setState((draft) => {
-            draft.skuPanelOpen = false
-          })
-        }}
-        onChange={(skuText, curItem) => {
-          setState((draft) => {
-            draft.skuText = skuText
-            draft.curItem = curItem
-          })
-        }}
-      />
-
-      <SpLogin ref={loginRef} />
-    </SpPage>
+    <View className=''>
+      {
+        keywords?"":<CompsCategoryOld></CompsCategoryOld>
+      }
+    </View>
   )
 }
 
