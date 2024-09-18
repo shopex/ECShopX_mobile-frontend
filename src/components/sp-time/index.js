72fee2c6c00420fd392d2f454211a400c92057ea
chenxin <398275325@qq.com>
2024-09-18 15:56:04 +0800

    优化sptime组件

--- src/components/sp-time/index.js
+++ src/components/sp-time/index.js
@@ -9,44 +9,45 @@ const initialState = {
   selector: ['按年', '按月', '按日'],
   selectorChecked: '按年',
   seleIndex: 0,
-  timeDay: '请选择'
+  timeDay: '请选择',
+  setPicker: true
 }
 
 function SpTime(props) {
   const [state, setState] = useImmer(initialState)
-  const { selector, selectorChecked, seleIndex, timeDay } = state
-  const {
-    onTimeChange = () => {},
-    onFormatChange = () => {},
-    selects = 0,
-    nowTimeDa = '请选择'
-  } = props
+  const { selector, selectorChecked, seleIndex, timeDay,setPicker } = state
+  const { onTimeChange = () => {}, selects = 0, nowTimeDa = '请选择' } = props
   const onChange = (e) => {
     setState((draft) => {
       draft.selectorChecked = selector[e.detail.value]
       draft.seleIndex = e.detail.value
       draft.timeDay = '请选择'
+      draft.setPicker = false
     })
-    onFormatChange()
   }
 
   useEffect(() => {
-    let time = nowTimeDa.toString().replace(/-/g,'/');
     setState((draft) => {
       draft.seleIndex = selects
       draft.selectorChecked = selector[selects]
-      draft.timeDay = time
+      draft.timeDay = nowTimeDa
     })
-    console.log(time, 'time111')
+    console.log('selects999999', timeDay)
   }, [])
 
+  useEffect(() => {
+    if(!setPicker){
+      setState((draft) => {
+        draft.setPicker = true
+      })
+    }
+  },[setPicker])
+
   const onDateChange = (e) => {
-    let time = e.detail.value.toString().replace(/-/g,'/');
     setState((draft) => {
-      draft.timeDay = time
+      draft.timeDay = e.detail.value
     })
     onTimeChange(seleIndex, e.detail.value)
-    console.log(time,'time222')
   }
 
   return (
@@ -58,16 +59,17 @@ function SpTime(props) {
             <Text className='iconfont icon-xialajiantou'></Text>
           </View>
         </Picker>
-
-        <Picker
-          mode='date'
-          fields={`${seleIndex == 0 ? 'year' : seleIndex == 1 ? 'month' : 'day'}`}
-          onChange={onDateChange}
-          className='specific-time'
-        >
-          <Text className='iconfont icon-riqi'></Text>
-          <Text>{timeDay}</Text>
-        </Picker>
+        {setPicker && (
+          <Picker
+            mode='date'
+            fields={`${seleIndex == 0 ? 'year' : seleIndex == 1 ? 'month' : 'day'}`}
+            onChange={onDateChange}
+            className='specific-time'
+          >
+            <Text className='iconfont icon-riqi'></Text>
+            <Text>{timeDay}</Text>
+          </Picker>
+        )}
       </View>
     </View>
   )
