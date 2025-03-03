dd8a3586e8f87c703da606f1612675d90f4d6a91
zhangjing <zhangjing@.shopexcn>
2025-03-03 18:28:11 +0800

    ECX-5810

--- src/subpages/trade/detail.js
+++ src/subpages/trade/detail.js
@@ -187,6 +187,8 @@ function TradeDetail(props) {
       setState((draft) => {
         draft.openWriteOffCode = true
       })
+    } else if (key == 'track') {
+      handleTrackDetail
     } else {
       action(info)
     }
@@ -399,11 +401,11 @@ function TradeDetail(props) {
                       拨打电话
                     </Text>
                   </View>
-                 { info?.receiptType !== 'merchant'&&<View>
+                <View>
                     <Text className='deliver-opreator-phone' onClick={handleTrackDetail}>
                       订单跟踪
                     </Text>
-                  </View>}
+                  </View>
                 </View>
               )}
             </View>
