616155457d0eaa12e98b03c4a714d183094eaced
qugao <qugao@shopex.cn>
2025-08-04 10:36:40 +0800

    feat: 开启达摩配置禁止修改手机号

--- src/subpages/member/user-info.js
+++ src/subpages/member/user-info.js
@@ -29,13 +29,15 @@ function MemberUserInfo() {
     checkboxPickerTitle,
     showCheckboxPicker,
     checkboxKey,
-    checkboxList
+    checkboxList,
+    dmcrm_is_open
   } = state
   const { userInfo = {} } = useSelector((_state) => _state.user)
   const pageRef = useRef()
   const { getUserInfo, logout } = useLogin()
 
   useEffect(() => {
+    getCommonSetting()
     getFormItem()
   }, [])
 
@@ -49,11 +51,19 @@ function MemberUserInfo() {
 
   // console.log('userInfo ******', userInfo)
 
+  const getCommonSetting = async () => {
+    const res = await api.category.getCommonSetting()
+    setState((draft) => {
+      draft.dmcrm_is_open = res?.dmcrm_is_open
+    })
+  }
   const getFormItem = async () => {
     const data = await api.user.regParam({
       is_edite_page: true
     })
-    const {other_params:{custom_data}} = userInfo
+    const {
+      other_params: { custom_data }
+    } = userInfo
     const _formItems = []
     const _formUserInfo = {
       avatar: userInfo.avatar,
@@ -61,22 +71,25 @@ function MemberUserInfo() {
       ...custom_data
     }
     Object.keys(data).forEach((key) => {
-    const value = custom_data?.[key] ||userInfo[key]
-    if (data[key].is_open) {
+      const value = custom_data?.[key] || userInfo[key]
+      if (data[key].is_open) {
         if (key !== 'mobile' && key !== 'username') {
           _formItems.push(data[key])
         }
       }
       if (data[key].element_type == 'checkbox') {
-        _formUserInfo[key] =  value&&value.length>0? value.split(',').map(item=>({name:item,ischecked:true})) : [];
+        _formUserInfo[key] =
+          value && value.length > 0
+            ? value.split(',').map((item) => ({ name: item, ischecked: true }))
+            : []
       } else {
         _formUserInfo[key] = value || ''
       }
-      if(key === 'sex'){
+      if (key === 'sex') {
         const sexType = {
-          0:'未知',
-          1:'男',
-          2:'女'
+          0: '未知',
+          1: '男',
+          2: '女'
         }
         _formUserInfo[key] = sexType[value]
       }
@@ -174,7 +187,7 @@ function MemberUserInfo() {
           'placeholder': formUserInfo[key].length == 0
         })}
         onClick={() => {
-            const _checkboxList = checkbox.map((item) => {
+          const _checkboxList = checkbox.map((item) => {
             const fd = formUserInfo[key].find((k) => k.name == item.name)
             const isChecked = fd ? fd.ischecked : false
             return {
@@ -348,15 +361,18 @@ function MemberUserInfo() {
           }
         ></SpCell>
         <SpCell
-          isLink
+          isLink={!dmcrm_is_open}
           title='手机号'
           value={userInfo?.mobile}
           onClick={() => {
+            if (dmcrm_is_open) {
+              return
+            }
             Taro.navigateTo({
               url: '/subpages/auth/edit-phone'
             })
           }}
-        ></SpCell>
+        />
       </View>
 
       <View className='block-container'>
