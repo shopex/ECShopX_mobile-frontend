f4fe78a8755e373d6e1fc412556d66259d495181
qugao <qugao@shopex.cn>
2025-08-19 17:05:24 +0800

    结算页申请开票显示错误

--- src/pages/cart/espier-checkout.js
+++ src/pages/cart/espier-checkout.js
@@ -1086,7 +1086,7 @@ function CartCheckout(props) {
             value={couponText || '请选择'}
           />
         )}
-        {(!bargain_id && totalInfo.invoice_status) ? (
+        {(totalInfo.invoice_status) ? (
           <SpCell
             isLink
             title='开发票'
